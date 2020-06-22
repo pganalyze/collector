c54a97f792457ad09f7aa965086ae53473b0b906
Lukas Fittl <lukas@fittl.com>
2020-06-21 23:31:52 -0700

    Log EXPLAIN: Don't run EXPLAIN logic when there are no query samples
    This saves us an unnecessary Postgres connection.

--- logs/stream/stream.go
+++ logs/stream/stream.go
@@ -229,7 +229,7 @@ func ProcessLogStream(server state.Server, logLines []state.LogLine, globalColle
 		return tooFreshLogLines
 	}
 
-	if server.Config.EnableLogExplain {
+	if server.Config.EnableLogExplain && len(logState.QuerySamples) != 0 {
 		db, err := postgres.EstablishConnection(server, prefixedLogger, globalCollectionOpts, "")
 		if err == nil {
 			logState.QuerySamples = postgres.RunExplain(db, server.Config.GetDbName(), logState.QuerySamples)
