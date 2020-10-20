afca831ef6ecb81f57196333bc9f2099207ba6de
Maciek Sakrejda <m.sakrejda@gmail.com>
2020-10-20 11:03:58 -0700

    Drop unnecessary log_line_prefix check
    This function is only called from one place where we already check the
    log_line_prefix beforehand and bail if it's not valid.

--- input/system/selfhosted/test_run.go
+++ input/system/selfhosted/test_run.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/pganalyze/collector/input/postgres"
-	"github.com/pganalyze/collector/logs"
 	"github.com/pganalyze/collector/state"
 	"github.com/pganalyze/collector/util"
 )
@@ -16,19 +15,10 @@ import (
 func TestLogTail(server *state.Server, globalCollectionOpts state.CollectionOpts, prefixedLogger *util.Logger) error {
 	cctx, cancel := context.WithCancel(context.Background())
 
-	logLinePrefix, err := getPostgresSetting("log_line_prefix", server, globalCollectionOpts, prefixedLogger)
-	if err != nil {
-		cancel()
-		return err
-	} else if !logs.IsSupportedPrefix(logLinePrefix) {
-		cancel()
-		return fmt.Errorf("Unsupported log_line_prefix setting: '%s'", logLinePrefix)
-	}
-
 	logTestSucceeded := make(chan bool, 1)
 
 	logStream := logReceiver(cctx, server, globalCollectionOpts, prefixedLogger, logTestSucceeded)
-	err = setupLogLocationTail(cctx, server.Config.LogLocation, logStream, prefixedLogger)
+	err := setupLogLocationTail(cctx, server.Config.LogLocation, logStream, prefixedLogger)
 	if err != nil {
 		cancel()
 		return err
