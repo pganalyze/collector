9d837ea573bc381a2e9ef3c0e1e7f80582fbd83c
Maciek Sakrejda <m.sakrejda@gmail.com>
2020-11-23 10:25:46 -0800

    Drop RestartPostgres TODO
    We are only supporting systemd platforms right now, and error output
    is accounted for.

--- setup/service/service.go
+++ setup/service/service.go
@@ -6,7 +6,6 @@ import (
 )
 
 func RestartPostgres() error {
-	// TODO: account for different environments; handle error output
 	cmd := exec.Command("systemctl", "restart", "postgresql")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
