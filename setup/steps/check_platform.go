f5d642a93dcba19a79daadcebe967076f49b0df4
Maciek Sakrejda <m.sakrejda@gmail.com>
2020-11-27 16:40:14 -0800

    Address review feedback

--- setup/steps/check_platform.go
+++ setup/steps/check_platform.go
@@ -2,6 +2,8 @@ package steps
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
 
 	s "github.com/pganalyze/collector/setup/state"
 	"github.com/shirou/gopsutil/host"
@@ -19,16 +21,21 @@ var CheckPlatform = &s.Step{
 		state.PlatformFamily = hostInfo.PlatformFamily
 		state.PlatformVersion = hostInfo.PlatformVersion
 
+		platVerNum, err := strconv.ParseFloat(state.PlatformVersion, 32)
+		if err != nil {
+			return false, fmt.Errorf("could not parse current platform version: %s / version %s", state.Platform, state.PlatformVersion)
+		}
+
 		if state.Platform == "ubuntu" {
-			if state.PlatformVersion < "14.04" {
+			if platVerNum < 14.04 {
 				return false, errors.New("Ubuntu versions older than 14.04 are not supported")
 			}
 		} else if state.Platform == "debian" {
-			if state.PlatformVersion < "10.0" {
+			if platVerNum < 10.0 {
 				return false, errors.New("Debian versions older than 10 are not supported")
 			}
 		} else {
-			return false, errors.New("this distribution is not currently supported; please contact support")
+			return false, fmt.Errorf("the current platform (%s) is not currently supported; please contact support", state.Platform)
 		}
 
 		return true, nil
