b34c179912ef69d93fadfd43dcaf19c5fd39dd07
Maciek Sakrejda <m.sakrejda@gmail.com>
2021-11-08 10:56:51 -0800

    Add support for multiple domain constraints

--- state/postgres_types.go
+++ state/postgres_types.go
@@ -7,15 +7,15 @@ type Xid uint32
 
 // PostgresType - User-defined custom data types
 type PostgresType struct {
-	Oid              Oid
-	DatabaseOid      Oid
-	SchemaName       string
-	Name             string
-	Type             string
-	DomainType       null.String
-	DomainNotNull    bool
-	DomainDefault    null.String
-	DomainConstraint null.String
-	EnumValues       []string
-	CompositeAttrs   [][2]string
+	Oid               Oid
+	DatabaseOid       Oid
+	SchemaName        string
+	Name              string
+	Type              string
+	DomainType        null.String
+	DomainNotNull     bool
+	DomainDefault     null.String
+	DomainConstraints []string
+	EnumValues        []string
+	CompositeAttrs    [][2]string
 }
