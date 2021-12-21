72128a63aac0f9e1b5a4b29023418f55c7b049e1
Lukas Fittl <lukas@fittl.com>
2021-12-20 23:57:40 -0800

    Fix small package building bugs / typos

--- packages/repo/sync_rpm.sh
+++ packages/repo/sync_rpm.sh
@@ -6,37 +6,37 @@ set -e
 printf "key 1\ndelkey\ny\nkey 1\ndelkey\ny\nsave\n" | gpg --batch --command-fd 0 --edit-key $REPO_GPG_KEY
 
 rpm --addsign /rpm/systemd/$RPM_PACKAGE_X86_64
-rpm --addsign /rpm/systemd/$RPM_PACKAGE_AMD64
+rpm --addsign /rpm/systemd/$RPM_PACKAGE_ARM64
 
 # Verify that we've actually correctly signed the packages
 rpm --import https://packages.pganalyze.com/pganalyze_signing_key.asc
 rpm --checksig /rpm/systemd/$RPM_PACKAGE_X86_64
-rpm --checksig /rpm/systemd/$RPM_PACKAGE_AMD64
+rpm --checksig /rpm/systemd/$RPM_PACKAGE_ARM64
 
 mkdir -p /repo/el/7/RPMS
 cp /rpm/systemd/$RPM_PACKAGE_X86_64 /repo/el/7/RPMS/
-cp /rpm/systemd/$RPM_PACKAGE_AMD64 /repo/el/7/RPMS/
+cp /rpm/systemd/$RPM_PACKAGE_ARM64 /repo/el/7/RPMS/
 createrepo --update /repo/el/7
 rm -f /repo/el/7/repodata/repomd.xml.asc
 gpg --detach-sign --armor --batch /repo/el/7/repodata/repomd.xml
 
 mkdir -p /repo/el/8/RPMS
 cp /rpm/systemd/$RPM_PACKAGE_X86_64 /repo/el/8/RPMS/
-cp /rpm/systemd/$RPM_PACKAGE_AMD64 /repo/el/8/RPMS/
+cp /rpm/systemd/$RPM_PACKAGE_ARM64 /repo/el/8/RPMS/
 createrepo --update /repo/el/8
 rm -f /repo/el/8/repodata/repomd.xml.asc
 gpg --detach-sign --armor --batch /repo/el/8/repodata/repomd.xml
 
 mkdir -p /repo/fedora/34/RPMS
 cp /rpm/systemd/$RPM_PACKAGE_X86_64 /repo/fedora/34/RPMS/
-cp /rpm/systemd/$RPM_PACKAGE_AMD64 /repo/fedora/34/RPMS/
+cp /rpm/systemd/$RPM_PACKAGE_ARM64 /repo/fedora/34/RPMS/
 createrepo --update /repo/fedora/34
 rm -f /repo/fedora/34/repodata/repomd.xml.asc
 gpg --detach-sign --armor --batch /repo/fedora/34/repodata/repomd.xml
 
 mkdir -p /repo/fedora/35/RPMS
 cp /rpm/systemd/$RPM_PACKAGE_X86_64 /repo/fedora/35/RPMS/
-cp /rpm/systemd/$RPM_PACKAGE_AMD64 /repo/fedora/35/RPMS/
+cp /rpm/systemd/$RPM_PACKAGE_ARM64 /repo/fedora/35/RPMS/
 createrepo --update /repo/fedora/35
 rm -f /repo/fedora/35/repodata/repomd.xml.asc
 gpg --detach-sign --armor --batch /repo/fedora/35/repodata/repomd.xml
