0ee36fe579003c2544f9fa97d02b5ce840a52667
Lukas Fittl <lukas@fittl.com>
2021-12-20 16:31:14 -0800

    Build packages for ARM64, add Debian 11 & update Fedora to 35/34 (#230)

--- packages/repo/sync_deb.sh
+++ packages/repo/sync_deb.sh
@@ -7,24 +7,35 @@ printf "key 1\ndelkey\ny\nkey 1\ndelkey\ny\nsave\n" | gpg --batch --command-fd 0
 
 mkdir -p /repo/ubuntu/xenial/conf
 cp /root/deb.distributions /repo/ubuntu/xenial/conf/distributions
-reprepro --basedir /repo/ubuntu/xenial includedeb stable /deb/systemd/$DEB_PACKAGE
+reprepro --basedir /repo/ubuntu/xenial includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
+reprepro --basedir /repo/ubuntu/xenial includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
 
 mkdir -p /repo/ubuntu/bionic/conf
 cp /root/deb.distributions /repo/ubuntu/bionic/conf/distributions
-reprepro --basedir /repo/ubuntu/bionic includedeb stable /deb/systemd/$DEB_PACKAGE
+reprepro --basedir /repo/ubuntu/bionic includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
+reprepro --basedir /repo/ubuntu/bionic includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
 
 mkdir -p /repo/ubuntu/focal/conf
 cp /root/deb.distributions /repo/ubuntu/focal/conf/distributions
-reprepro --basedir /repo/ubuntu/focal includedeb stable /deb/systemd/$DEB_PACKAGE
+reprepro --basedir /repo/ubuntu/focal includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
+reprepro --basedir /repo/ubuntu/focal includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
 
 mkdir -p /repo/debian/jessie/conf
 cp /root/deb.distributions /repo/debian/jessie/conf/distributions
-reprepro --basedir /repo/debian/jessie includedeb stable /deb/systemd/$DEB_PACKAGE
+reprepro --basedir /repo/debian/jessie includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
+reprepro --basedir /repo/debian/jessie includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
 
 mkdir -p /repo/debian/stretch/conf
 cp /root/deb.distributions /repo/debian/stretch/conf/distributions
-reprepro --basedir /repo/debian/stretch includedeb stable /deb/systemd/$DEB_PACKAGE
+reprepro --basedir /repo/debian/stretch includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
+reprepro --basedir /repo/debian/stretch includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
 
 mkdir -p /repo/debian/buster/conf
 cp /root/deb.distributions /repo/debian/buster/conf/distributions
-reprepro --basedir /repo/debian/buster includedeb stable /deb/systemd/$DEB_PACKAGE
+reprepro --basedir /repo/debian/buster includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
+reprepro --basedir /repo/debian/buster includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
+
+mkdir -p /repo/debian/bullseye/conf
+cp /root/deb.distributions /repo/debian/bullseye/conf/distributions
+reprepro --basedir /repo/debian/bullseye includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
+reprepro --basedir /repo/debian/bullseye includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
