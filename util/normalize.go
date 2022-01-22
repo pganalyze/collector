0c4de11147fe595f01044a430f9f43a9f99f7185
Lukas Fittl <lukas@fittl.com>
2022-01-22 12:39:29 -0800

    Normalization: Attempt auto-fixing truncated queries
    This adjusts the normalization logic to use the same "fix unparsable
    queries" logic that already existed for fingerprinting, and is mainly
    intended for data in pg_stat_activity (that is often truncated).
    
    This logic provides a better level of grouping for these truncated
    queries.
    
    Add the new "/* truncated-query */ " magic comment to make it clear to the
    viewer that they are looking at a fixed truncated query text, and not
    the full query text as sent by the application.

--- util/normalize.go
+++ util/normalize.go
@@ -4,12 +4,22 @@ import (
 	pg_query "github.com/pganalyze/pg_query_go/v2"
 )
 
+// TruncatedQueryMarker - Added to queries that were truncated and caused a
+// parsing error, but we were able to "fix" to allow grouping in the UI
+const TruncatedQueryMarker string = "/* truncated-query */ "
+
 // NormalizeQuery - Normalizes the query text with an improved variant of the
 // pg_stat_statements normalization logic.
 func NormalizeQuery(query string, filterQueryText string, trackActivityQuerySize int) string {
 	normalizedQuery, err := pg_query.Normalize(query)
 	if err == nil {
 		return normalizedQuery
+	}
+
+	fixedQuery := fixTruncatedQuery(query)
+	normalizedQuery, err = pg_query.Normalize(fixedQuery)
+	if err == nil {
+		return TruncatedQueryMarker + normalizedQuery
 	}
 
 	if filterQueryText == "none" {
