10b2c7c7f86077890a7872b26d3649cf2c3e5a36
Lukas Fittl <lukas@fittl.com>
2022-05-13 00:30:38 -0700

    Add docs page link to "Unsupported log_line_prefix setting" error

--- runner/logs.go
+++ runner/logs.go
@@ -323,6 +323,7 @@ func TestLogsForAllServers(servers []*state.Server, globalCollectionOpts state.C
 			continue
 		} else if !logs.IsSupportedPrefix(logLinePrefix) && !(server.Config.SystemType == "heroku" && logLinePrefix == logs.HerokuLogLinePrefix) {
 			prefixedLogger.PrintError("ERROR - Unsupported log_line_prefix setting: '%s'", logLinePrefix)
+			prefixedLogger.PrintInfo("HINT - You can find a list of supported settings in the pganalyze documentation: https://pganalyze.com/docs/log-insights/setup/self-managed/troubleshooting")
 			hasFailedServers = true
 			continue
 		}
