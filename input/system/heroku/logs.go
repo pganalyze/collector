pganalyze/collector

commit 735d95cc4e6d4ab012a2a5a92bc282bd180c2fb1

Refactor log runner, use consistent 10s interval for streamed logs
This is a major refactoring to reduce a lot of duplicate Go routine
code in the log streaming logic, and centralizes all top-level log
handling in the runner package, which then calls input/logs/output
packages as needed.

Additionally, change the interval for streamed logs from (at most) once
every 3 seconds to every 10 seconds. Its unnecessary to send more often
and can cause tiny log file records to be created that put unnecessary
stress on the system.

For consistency, drop the scheduler logic use for log downloads, and
use a simple time.Ticker to schedule log downloads every 30 seconds, like
before.

input/system/heroku/logs.go
@@ -1,17 +1,17 @@
 package heroku
 
 import (
+	"context"
 	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/bmizerany/lpx"
 	"github.com/kr/logfmt"
 	"github.com/pganalyze/collector/grant"
-	"github.com/pganalyze/collector/logs"
-	"github.com/pganalyze/collector/logs/stream"
 	"github.com/pganalyze/collector/output"
 	"github.com/pganalyze/collector/output/pganalyze_collector"
 	"github.com/pganalyze/collector/state"
@@ -20,9 +20,9 @@ import (
 )
 
 type HerokuLogStreamItem struct {
-	Header    lpx.Header
-	Content   []byte
-	Namespace string
+	Header  lpx.Header
+	Content []byte
+	Path    string
 }
 
 type SystemSample struct {
@@ -39,28 +39,20 @@ type SystemSample struct {
 	WriteIops         float64 `logfmt:"sample#write-iops"`
 }
 
-func SetupLogReceiver(servers []*state.Server, globalCollectionOpts state.CollectionOpts, logger *util.Logger, herokuLogStream <-chan HerokuLogStreamItem) {
-	go logReceiver(servers, herokuLogStream, globalCollectionOpts, logger)
-
-	for _, server := range servers {
-		logs.EmitTestLogMsg(server, globalCollectionOpts, logger)
-	}
-}
-
-func catchIdentifyServerLine(sourceName string, content string, nameToServer map[string]*state.Server, servers []*state.Server) map[string]*state.Server {
+func catchIdentifyServerLine(sourceName string, content string, sourceToServer map[string]*state.Server, servers []*state.Server) map[string]*state.Server {
 	identifyParts := regexp.MustCompile(`^pganalyze-collector-identify: ([\w_]+)`).FindStringSubmatch(content)
 	if len(identifyParts) == 2 {
 		for _, server := range servers {
 			if server.Config.SectionName == identifyParts[1] {
-				nameToServer[sourceName] = server
+				sourceToServer[sourceName] = server
 			}
 		}
 	}
 
-	return nameToServer
+	return sourceToServer
 }
 
-func processSystemMetrics(timestamp time.Time, content []byte, nameToServer map[string]*state.Server, globalCollectionOpts state.CollectionOpts, logger *util.Logger, namespace string) {
+func processSystemMetrics(timestamp time.Time, content []byte, sourceToServer map[string]*state.Server, globalCollectionOpts state.CollectionOpts, logger *util.Logger, namespace string) {
 	var sample SystemSample
 	err := logfmt.Unmarshal(content, &sample)
 	if err != nil {
@@ -71,7 +63,7 @@ func processSystemMetrics(timestamp time.Time, content []byte, nameToServer map[
 	if !strings.HasPrefix(sourceName, "HEROKU_POSTGRESQL_") {
 		sourceName = "HEROKU_POSTGRESQL_" + sourceName
 	}
-	server, exists := nameToServer[namespace+" / "+sourceName]
+	server, exists := sourceToServer[namespace+" / "+sourceName]
 	if !exists {
 		logger.PrintInfo("Ignoring system data since server can't be matched yet - if this keeps showing up you have a configuration error for %s", namespace+" / "+sourceName)
 		return
@@ -137,10 +129,10 @@ func processSystemMetrics(timestamp time.Time, content []byte, nameToServer map[
 	return
 }
 
-func makeLogLine(timestamp time.Time, backendPid int64, logLineNumber int64, logLineNumberChunk int64, logLevel string, content string, nameToServer map[string]*state.Server) *state.LogLine {
+func makeLogLine(timestamp time.Time, backendPid int64, logLineNumber int64, logLineNumberChunk int64, logLevel string, content string, now time.Time) *state.LogLine {
 	var logLine state.LogLine
 
-	logLine.CollectedAt = time.Now()
+	logLine.CollectedAt = now
 	logLine.OccurredAt = timestamp
 	logLine.BackendPid = int32(backendPid)
 	logLine.LogLineNumber = int32(logLineNumber)
@@ -155,86 +147,96 @@ func makeLogLine(timestamp time.Time, backendPid int64, logLineNumber int64, log
 	return &logLine
 }
 
-func logStreamItemToLogLine(item HerokuLogStreamItem, servers []*state.Server, nameToServer map[string]*state.Server, globalCollectionOpts state.CollectionOpts, logger *util.Logger) (map[string]*state.Server, *state.LogLine, string) {
+func logStreamItemToLogLine(item HerokuLogStreamItem, servers []*state.Server, sourceToServer map[string]*state.Server, now time.Time, globalCollectionOpts state.CollectionOpts, logger *util.Logger) (map[string]*state.Server, *state.LogLine, string) {
 	timestamp, err := time.Parse(time.RFC3339, string(item.Header.Time))
 	if err != nil {
-		return nameToServer, nil, ""
+		return sourceToServer, nil, ""
 	}
 
 	if string(item.Header.Name) != "app" {
-		return nameToServer, nil, ""
+		return sourceToServer, nil, ""
+	}
+
+	namespace := "default"
+	if strings.HasPrefix(item.Path, "/logs/") {
+		namespace = strings.Replace(item.Path, "/logs/", "", 1)
 	}
 
 	if string(item.Header.Procid) == "heroku-postgres" {
-		processSystemMetrics(timestamp, item.Content, nameToServer, globalCollectionOpts, logger, item.Namespace)
-		return nameToServer, nil, ""
+		processSystemMetrics(timestamp, item.Content, sourceToServer, globalCollectionOpts, logger, namespace)
+		return sourceToServer, nil, ""
 	}
 
 	parts := regexp.MustCompile(`^postgres.(\d+)`).FindStringSubmatch(string(item.Header.Procid))
 	if len(parts) != 2 {
-		return nameToServer, nil, ""
+		return sourceToServer, nil, ""
 	}
 	backendPid, _ := strconv.ParseInt(parts[1], 10, 32)
 
 	contentParts := regexp.MustCompile(`^\[(\w+)\] \[(\d+)-(\d+)\] ( sql_error_code = \w+ (\w+):  )?(.+)`).FindStringSubmatch(string(item.Content))
 	if len(contentParts) != 7 {
 		fmt.Printf("ERR: %s\n", string(item.Content))
-		return nameToServer, nil, ""
+		return sourceToServer, nil, ""
 	}
 
 	sourceName := contentParts[1]
 	if !strings.HasPrefix(sourceName, "HEROKU_POSTGRESQL_") {
 		sourceName = "HEROKU_POSTGRESQL_" + sourceName
 	}
-	sourceName = item.Namespace + " / " + sourceName
+	sourceName = namespace + " / " + sourceName
 	logLineNumber, _ := strconv.ParseInt(contentParts[2], 10, 32)
 	logLineNumberChunk, _ := strconv.ParseInt(contentParts[3], 10, 32)
 	logLevel := contentParts[5]
 	content := contentParts[6]
 
-	nameToServer = catchIdentifyServerLine(sourceName, content, nameToServer, servers)
+	sourceToServer = catchIdentifyServerLine(sourceName, content, sourceToServer, servers)
 
-	newLogLine := makeLogLine(timestamp, backendPid, logLineNumber, logLineNumberChunk, logLevel, content, nameToServer)
+	newLogLine := makeLogLine(timestamp, backendPid, logLineNumber, logLineNumberChunk, logLevel, content, now)
 
-	return nameToServer, newLogLine, sourceName
+	return sourceToServer, newLogLine, sourceName
 }
 
-func logReceiver(servers []*state.Server, in <-chan HerokuLogStreamItem, globalCollectionOpts state.CollectionOpts, logger *util.Logger) {
-	var logLinesByName map[string][]state.LogLine
-	var nameToServer map[string]*state.Server
+func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*state.Server, in <-chan HerokuLogStreamItem, out chan state.ParsedLogStreamItem, globalCollectionOpts state.CollectionOpts, logger *util.Logger) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 
-	logLinesByName = make(map[string][]state.LogLine)
-	nameToServer = make(map[string]*state.Server)
+		logLinesBySource := make(map[string][]state.LogLine)
+		sourceToServer := make(map[string]*state.Server)
 
-	for {
-		item, ok := <-in
-		if !ok {
-			return
-		}
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case item, ok := <-in:
+				if !ok {
+					return
+				}
 
-		var newLogLine *state.LogLine
-		var sourceName string
-		nameToServer, newLogLine, sourceName = logStreamItemToLogLine(item, servers, nameToServer, globalCollectionOpts, logger)
-		if newLogLine != nil && sourceName != "" {
-			logLinesByName[sourceName] = append(logLinesByName[sourceName], *newLogLine)
-		}
+				now := time.Now()
 
-		for sourceName, logLines := range logLinesByName {
-			server, exists := nameToServer[sourceName]
-			if !exists {
-				logger.PrintInfo("Ignoring log line since server can't be matched yet - if this keeps showing up you have a configuration error for %s", sourceName)
-				logLinesByName[sourceName] = []state.LogLine{}
-				continue
+				var newLogLine *state.LogLine
+				var sourceName string
+				sourceToServer, newLogLine, sourceName = logStreamItemToLogLine(item, servers, sourceToServer, now, globalCollectionOpts, logger)
+				if newLogLine != nil && sourceName != "" {
+					logLinesBySource[sourceName] = append(logLinesBySource[sourceName], *newLogLine)
+				}
+
+				for sourceName, logLines := range logLinesBySource {
+					server, exists := sourceToServer[sourceName]
+					if !exists {
+						logger.PrintInfo("Ignoring log line since server can't be matched yet - if this keeps showing up you have a configuration error for %s", sourceName)
+						logLinesBySource[sourceName] = []state.LogLine{}
+						continue
+					}
+
+					for _, logLine := range logLines {
+						logLine.Username = server.Config.GetDbUsername()
+						logLine.Database = server.Config.GetDbName()
+						out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
+					}
+				}
 			}
-
-			for idx, logLine := range logLines {
-				logLine.Username = server.Config.GetDbUsername()
-				logLine.Database = server.Config.GetDbName()
-				logLines[idx] = logLine
-			}
-
-			prefixedLogger := logger.WithPrefix(server.Config.SectionName)
-			logLinesByName[sourceName] = stream.ProcessLogStream(server, logLines, globalCollectionOpts, prefixedLogger, nil, stream.LogTestNone)
 		}
-	}
+	}()
 }

input/logs.go
@@ -1,28 +1,0 @@
-package input
-
-import (
-	"time"
-
-	"github.com/pganalyze/collector/input/postgres"
-	"github.com/pganalyze/collector/input/system"
-	"github.com/pganalyze/collector/state"
-	"github.com/pganalyze/collector/util"
-)
-
-// DownloadLogs - Downloads a "logs" snapshot of log data we need on a regular interval
-func DownloadLogs(server *state.Server, prevLogState state.PersistedLogState, collectionOpts state.CollectionOpts, logger *util.Logger) (tls state.TransientLogState, pls state.PersistedLogState, err error) {
-	var querySamples []state.PostgresQuerySample
-
-	tls.CollectedAt = time.Now()
-	pls, tls.LogFiles, querySamples, err = system.DownloadLogFiles(prevLogState, server.Config, logger)
-	if err != nil {
-		return
-	}
-
-	if server.Config.EnableLogExplain {
-		tls.QuerySamples = postgres.RunExplain(server, querySamples, collectionOpts, logger)
-	} else {
-		tls.QuerySamples = querySamples
-	}
-	return
-}

input/system/azure/logs.go
@@ -7,16 +7,46 @@ import (
 	"regexp"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/pganalyze/collector/config"
+	"github.com/pganalyze/collector/logs"
 	"github.com/pganalyze/collector/state"
 	"github.com/pganalyze/collector/util"
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/Azure/azure-amqp-common-go/v3/aad"
 	eventhubs "github.com/Azure/azure-event-hubs-go/v3"
 	"github.com/Azure/azure-event-hubs-go/v3/persist"
 	"github.com/Azure/go-autorest/autorest/azure"
 )
+
+type AzurePostgresLogMessage struct {
+	Prefix       string `json:"prefix"`
+	Message      string `json:"message"`
+	Detail       string `json:"detail"`
+	ErrorLevel   string `json:"errorLevel"`
+	Domain       string `json:"domain"`
+	SchemaName   string `json:"schemaName"`
+	TableName    string `json:"tableName"`
+	ColumnName   string `json:"columnName"`
+	DatatypeName string `json:"datatypeName"`
+}
+
+type AzurePostgresLogRecord struct {
+	LogicalServerName string                  `json:"LogicalServerName"`
+	SubscriptionID    string                  `json:"SubscriptionId"`
+	ResourceGroup     string                  `json:"ResourceGroup"`
+	Time              string                  `json:"time"`
+	ResourceID        string                  `json:"resourceId"`
+	Category          string                  `json:"category"`
+	OperationName     string                  `json:"operationName"`
+	Properties        AzurePostgresLogMessage `json:"properties"`
+}
+
+type AzureEventHubData struct {
+	Records []AzurePostgresLogRecord `json:"records"`
+}
 
 var connectionReceivedRegexp = regexp.MustCompile(`^(connection received: host=[^ ]+( port=\w+)?) pid=\d+`)
 var connectionAuthorizedRegexp = regexp.MustCompile(`^(connection authorized: user=\w+)(database=\w+)`)
@@ -114,7 +144,10 @@ func setupEventHubReceiver(ctx context.Context, wg *sync.WaitGroup, logger *util
 	return nil
 }
 
-func SetupLogSubscriber(ctx context.Context, wg *sync.WaitGroup, globalCollectionOpts state.CollectionOpts, logger *util.Logger, servers []*state.Server, azureLogStream chan AzurePostgresLogRecord) error {
+func SetupLogSubscriber(ctx context.Context, wg *sync.WaitGroup, globalCollectionOpts state.CollectionOpts, logger *util.Logger, servers []*state.Server, parsedLogStream chan state.ParsedLogStreamItem) error {
+	azureLogStream := make(chan AzurePostgresLogRecord, state.LogStreamBufferLen)
+	setupLogTransformer(ctx, wg, servers, azureLogStream, parsedLogStream, globalCollectionOpts, logger)
+
 	// This map is used to avoid duplicate receivers to the same Azure Event Hub
 	eventHubReceivers := make(map[string]bool)
 
@@ -139,4 +172,47 @@ func SetupLogSubscriber(ctx context.Context, wg *sync.WaitGroup, globalCollectio
 	}
 
 	return nil
+}
+
+func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*state.Server, in <-chan AzurePostgresLogRecord, out chan state.ParsedLogStreamItem, globalCollectionOpts state.CollectionOpts, logger *util.Logger) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		// Only ingest log lines that were written in the last minute before startup
+		// (Azure Event Hub will return older log lines as well)
+		linesNewerThan := time.Now().Add(-1 * time.Minute)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case in, ok := <-in:
+				if !ok {
+					return
+				}
+
+				logLineContent := fmt.Sprintf("%s%s:  %s", in.Properties.Prefix, in.Properties.ErrorLevel, in.Properties.Message)
+				logLine, ok := logs.ParseLogLineWithPrefix("", logLineContent)
+				if !ok {
+					logger.PrintError("Can't parse log line: \"%s\"", logLineContent)
+					continue
+				}
+				logLine.CollectedAt = time.Now()
+				logLine.UUID = uuid.NewV4()
+
+				// Ignore loglines which are outside our time window (except in test runs)
+				if !logLine.OccurredAt.IsZero() && logLine.OccurredAt.Before(linesNewerThan) && !globalCollectionOpts.TestRun {
+					continue
+				}
+
+				for _, server := range servers {
+					if in.LogicalServerName == server.Config.AzureDbServerName {
+						out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
+					}
+				}
+
+			}
+		}
+	}()
 }

input/system/google_cloudsql/logs.go
@@ -12,6 +12,7 @@ import (
 	"google.golang.org/api/option"
 
 	"github.com/pganalyze/collector/config"
+	"github.com/pganalyze/collector/logs"
 	"github.com/pganalyze/collector/state"
 	"github.com/pganalyze/collector/util"
 )
@@ -29,6 +30,13 @@ type googleLogMessage struct {
 	Severity         string            `json:"severity"`
 	TextPayload      string            `json:"textPayload"`
 	Timestamp        string            `json:"timestamp"`
+}
+
+type LogStreamItem struct {
+	GcpProjectID          string
+	GcpCloudSQLInstanceID string
+	OccurredAt            time.Time
+	Content               string
 }
 
 func setupPubSubSubscriber(ctx context.Context, wg *sync.WaitGroup, logger *util.Logger, config config.ServerConfig, gcpLogStream chan LogStreamItem) error {
@@ -100,7 +108,10 @@ func setupPubSubSubscriber(ctx context.Context, wg *sync.WaitGroup, logger *util
 	return nil
 }
 
-func SetupLogSubscriber(ctx context.Context, wg *sync.WaitGroup, globalCollectionOpts state.CollectionOpts, logger *util.Logger, servers []*state.Server, gcpLogStream chan LogStreamItem) error {
+func SetupLogSubscriber(ctx context.Context, wg *sync.WaitGroup, globalCollectionOpts state.CollectionOpts, logger *util.Logger, servers []*state.Server, parsedLogStream chan state.ParsedLogStreamItem) error {
+	gcpLogStream := make(chan LogStreamItem, state.LogStreamBufferLen)
+	setupLogTransformer(ctx, wg, servers, gcpLogStream, parsedLogStream, logger)
+
 	// This map is used to avoid duplicate receivers to the same subscriber
 	gcpPubSubHandlers := make(map[string]bool)
 
@@ -126,4 +137,46 @@ func SetupLogSubscriber(ctx context.Context, wg *sync.WaitGroup, globalCollectio
 	}
 
 	return nil
+}
+
+func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*state.Server, in <-chan LogStreamItem, out chan state.ParsedLogStreamItem, logger *util.Logger) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		// Only ingest log lines that were written in the last minute before startup
+		linesNewerThan := time.Now().Add(-1 * time.Minute)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case in, ok := <-in:
+				if !ok {
+					return
+				}
+
+				// Note that we need to restore the original trailing newlines since
+				// ProcessLogStream below expects them and they are not present in the GCP
+				// log stream.
+				logLine, ok := logs.ParseLogLineWithPrefix("", in.Content+"\n")
+				if !ok {
+					logger.PrintError("Can't parse log line: \"%s\"", in.Content)
+					continue
+				}
+				logLine.OccurredAt = in.OccurredAt
+
+				// Ignore loglines which are outside our time window
+				if !logLine.OccurredAt.IsZero() && logLine.OccurredAt.Before(linesNewerThan) {
+					continue
+				}
+
+				for _, server := range servers {
+					if in.GcpProjectID == server.Config.GcpProjectID && in.GcpCloudSQLInstanceID == server.Config.GcpCloudSQLInstanceID {
+						out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
+					}
+				}
+			}
+		}
+	}()
 }

input/system/selfhosted/logs.go
@@ -14,13 +14,13 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/papertrail/go-tail/follower"
 	"github.com/pganalyze/collector/input/postgres"
 	"github.com/pganalyze/collector/logs"
-	"github.com/pganalyze/collector/logs/stream"
 	"github.com/pganalyze/collector/state"
 	"github.com/pganalyze/collector/util"
 	uuid "github.com/satori/go.uuid"
@@ -125,19 +125,23 @@ func DiscoverLogLocation(servers []*state.Server, globalCollectionOpts state.Col
 	}
 }
 
+func SetupLogTailForServer(ctx context.Context, wg *sync.WaitGroup, globalCollectionOpts state.CollectionOpts, logger *util.Logger, server *state.Server, parsedLogStream chan state.ParsedLogStreamItem) error {
+	if globalCollectionOpts.DebugLogs || globalCollectionOpts.TestRun {
+		logger.PrintInfo("Setting up log tail for %s", server.Config.LogLocation)
+	}
+
+	logStream := setupLogTransformer(ctx, wg, server, globalCollectionOpts, logger, parsedLogStream)
+	return setupLogLocationTail(ctx, server.Config.LogLocation, logStream, logger)
+}
+
 // SetupLogTails - Sets up continuously running log tails for all servers with a
 // local log directory or file specified
-func SetupLogTails(ctx context.Context, servers []*state.Server, globalCollectionOpts state.CollectionOpts, logger *util.Logger) {
+func SetupLogTails(ctx context.Context, wg *sync.WaitGroup, globalCollectionOpts state.CollectionOpts, logger *util.Logger, servers []*state.Server, parsedLogStream chan state.ParsedLogStreamItem) {
 	for _, server := range servers {
 		prefixedLogger := logger.WithPrefix(server.Config.SectionName)
 
 		if server.Config.LogLocation != "" {
-			if globalCollectionOpts.DebugLogs || globalCollectionOpts.TestRun {
-				prefixedLogger.PrintInfo("Setting up log tail for %s", server.Config.LogLocation)
-			}
-
-			logStream := logReceiver(ctx, server, globalCollectionOpts, prefixedLogger, nil)
-			err := setupLogLocationTail(ctx, server.Config.LogLocation, logStream, prefixedLogger)
+			err := SetupLogTailForServer(ctx, wg, globalCollectionOpts, logger, server, parsedLogStream)
 			if err != nil {
 				prefixedLogger.PrintError("ERROR - %s", err)
 			}
@@ -146,13 +150,13 @@ func SetupLogTails(ctx context.Context, servers []*state.Server, globalCollectio
 				prefixedLogger.PrintInfo("Setting up docker logs tail for %s", server.Config.LogDockerTail)
 			}
 
-			logStream := logReceiver(ctx, server, globalCollectionOpts, prefixedLogger, nil)
+			logStream := setupLogTransformer(ctx, wg, server, globalCollectionOpts, prefixedLogger, parsedLogStream)
 			err := setupDockerTail(ctx, server.Config.LogDockerTail, logStream, prefixedLogger)
 			if err != nil {
 				prefixedLogger.PrintError("ERROR - %s", err)
 			}
 		} else if server.Config.LogSyslogServer != "" {
-			logStream := logReceiver(ctx, server, globalCollectionOpts, prefixedLogger, nil)
+			logStream := setupLogTransformer(ctx, wg, server, globalCollectionOpts, prefixedLogger, parsedLogStream)
 			err := setupSyslogHandler(ctx, server.Config.LogSyslogServer, logStream, prefixedLogger)
 			if err != nil {
 				prefixedLogger.PrintError("ERROR - %s", err)
@@ -258,6 +262,7 @@ func setupLogLocationTail(ctx context.Context, logLocation string, out chan<- Se
 			tailCtx, tailCancel := context.WithCancel(ctx)
 			err = tailFile(tailCtx, fileName, out, prefixedLogger)
 			if err != nil {
+				tailCancel()
 				prefixedLogger.PrintError("ERROR - %s", err)
 			} else {
 				openFiles[fileName] = tailCancel
@@ -367,24 +372,17 @@ func setupDockerTail(ctx context.Context, containerName string, out chan<- SelfH
 	return nil
 }
 
-func logReceiver(ctx context.Context, server *state.Server, globalCollectionOpts state.CollectionOpts, prefixedLogger *util.Logger, logTestSucceeded chan<- bool) chan<- SelfHostedLogStreamItem {
+func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, server *state.Server, globalCollectionOpts state.CollectionOpts, prefixedLogger *util.Logger, parsedLogStream chan state.ParsedLogStreamItem) chan<- SelfHostedLogStreamItem {
 	logStream := make(chan SelfHostedLogStreamItem)
 
+	wg.Add(1)
 	go func() {
-		var logLines []state.LogLine
+		defer wg.Done()
 
 		// Only ingest log lines that were written in the last minute before startup,
 		// or later, so we avoid resending full large files on collector restarts
 		// TODO: Use prevState here instead to get the last logline we saw
 		linesNewerThan := time.Now().Add(-1 * time.Minute)
-
-		// Use a timeout to clear out loglines that don't have any follow-on lines
-		// (the threshold used in logs.ProcessLogStream is 3 seconds)
-		timeout := make(chan bool, 1)
-		go func() {
-			time.Sleep(3 * time.Second)
-			timeout <- true
-		}()
 
 		for {
 			select {
@@ -416,22 +414,11 @@ func logReceiver(ctx context.Context, server *state.Server, globalCollectionOpts
 				}
 
 				// Ignore loglines which are outside our time window
-				nullTime := time.Time{}
-				if logLine.OccurredAt != nullTime && logLine.OccurredAt.Before(linesNewerThan) {
+				if !logLine.OccurredAt.IsZero() && logLine.OccurredAt.Before(linesNewerThan) {
 					continue
 				}
 
-				logLines = append(logLines, logLine)
-			case <-timeout:
-				if len(logLines) > 0 {
-					logLines = stream.ProcessLogStream(server, logLines, globalCollectionOpts, prefixedLogger, logTestSucceeded, stream.LogTestCollectorIdentify)
-				}
-				go func() {
-					time.Sleep(3 * time.Second)
-					timeout <- true
-				}()
-			case <-ctx.Done():
-				return
+				parsedLogStream <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
 			}
 		}
 	}()

commit 1987de65d11a159490db28da0d6f470e988249eb

Heroku log processing: Avoid repeating the same line over and over again
This is a bug that was introduced in the refactoring in
735d95cc4e6d4ab012a2a5a92bc282bd180c2fb1, which kept the old logic that
remembers log lines across processing iterations. That is no longer needed
since we now handle that the same way for all streamed log providers in
setupLogStreamer.

input/system/heroku/logs.go
@@ -201,7 +201,6 @@ func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*sta
 	go func() {
 		defer wg.Done()
 
-		logLinesBySource := make(map[string][]state.LogLine)
 		sourceToServer := make(map[string]*state.Server)
 
 		for {
@@ -215,27 +214,22 @@ func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*sta
 
 				now := time.Now()
 
-				var newLogLine *state.LogLine
+				var logLine *state.LogLine
 				var sourceName string
-				sourceToServer, newLogLine, sourceName = logStreamItemToLogLine(item, servers, sourceToServer, now, globalCollectionOpts, logger)
-				if newLogLine != nil && sourceName != "" {
-					logLinesBySource[sourceName] = append(logLinesBySource[sourceName], *newLogLine)
+				sourceToServer, logLine, sourceName = logStreamItemToLogLine(item, servers, sourceToServer, now, globalCollectionOpts, logger)
+				if logLine == nil || sourceName == "" {
+					continue
 				}
 
-				for sourceName, logLines := range logLinesBySource {
-					server, exists := sourceToServer[sourceName]
-					if !exists {
-						logger.PrintInfo("Ignoring log line since server can't be matched yet - if this keeps showing up you have a configuration error for %s", sourceName)
-						logLinesBySource[sourceName] = []state.LogLine{}
-						continue
-					}
-
-					for _, logLine := range logLines {
-						logLine.Username = server.Config.GetDbUsername()
-						logLine.Database = server.Config.GetDbName()
-						out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
-					}
+				server, exists := sourceToServer[sourceName]
+				if !exists {
+					logger.PrintInfo("Ignoring log line since server can't be matched yet - if this keeps showing up you have a configuration error for %s", sourceName)
+					continue
 				}
+
+				logLine.Username = server.Config.GetDbUsername()
+				logLine.Database = server.Config.GetDbName()
+				out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: *logLine}
 			}
 		}
 	}()