fd65610fd60b0aaa2b2784ba23bb7f7e09c7f71e
Maciek Sakrejda <m.sakrejda@gmail.com>
2022-06-28 11:09:17 -0700

    Add RDS instance role hint when NoCredentialProviders error is hit (#286)

--- runner/logs.go
+++ runner/logs.go
@@ -122,6 +122,10 @@ func downloadLogsForServerWithLocksAndCallbacks(wg *sync.WaitGroup, server *stat
 	if err != nil {
 		server.LogStateMutex.Unlock()
 		prefixedLogger.PrintError("Could not collect logs for server: %s", err)
+		msg := err.Error()
+		if server.Config.SystemType == "amazon_rds" && strings.Contains(msg, "NoCredentialProviders") {
+			prefixedLogger.PrintInfo("HINT - This may occur if you have not assigned an IAM role to the collector EC2 instance, and have not provided AWS credentials through another method")
+		}
 		if server.Config.ErrorCallback != "" {
 			go runCompletionCallback("error", server.Config.ErrorCallback, server.Config.SectionName, "logs", err, prefixedLogger)
 		}
