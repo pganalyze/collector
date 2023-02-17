ba68e0f8a2d77bed9fcc96aef6a64e913551380c
Keiko Oda <keiko713@gmail.com>
2023-02-17 12:01:16 +0900

    Start collecting xmin horizon in full snapshot

--- input/postgres/server_stats.go
+++ input/postgres/server_stats.go
@@ -21,11 +21,46 @@ SELECT
 FROM pg_catalog.pg_control_checkpoint()
 `
 
+const xminHorizonSQL string = `
+SELECT
+(
+	SELECT
+		backend_xmin
+	FROM pg_stat_activity
+	WHERE backend_xmin IS NOT NULL OR backend_xid IS NOT NULL
+	ORDER BY greatest(age(backend_xmin), age(backend_xid)) DESC
+	LIMIT 1
+) as backend,
+(
+	SELECT
+		xmin
+	FROM pg_replication_slots
+	WHERE xmin IS NOT NULL
+	ORDER BY age(xmin) DESC
+	LIMIT 1
+) as replication_slot,
+(
+	SELECT
+		transaction AS xmin
+	FROM pg_prepared_xacts
+	ORDER BY age(transaction) DESC
+	LIMIT 1
+) as prepare_xact,
+(
+	SELECT
+		backend_xmin
+	FROM pg_stat_replication
+	WHERE backend_xmin IS NOT NULL
+	ORDER BY age(backend_xmin) DESC
+	LIMIT 1
+) as standby
+`
+
 func GetServerStats(logger *util.Logger, db *sql.DB, postgresVersion state.PostgresVersion, systemType string) (state.PostgresServerStats, error) {
 	var stats state.PostgresServerStats
 	var transactionIdSQL string
 
-	// Only collect xact ID related stats with non-replicas
+	// Only collect transaction ID or xmin horizon related stats with non-replicas
 	if isReplica, err := getIsReplica(db); err == nil && !isReplica {
 		if postgresVersion.Numeric >= state.PostgresVersion13 {
 			transactionIdSQL = transactionIdSQLPg13
@@ -35,6 +70,14 @@ func GetServerStats(logger *util.Logger, db *sql.DB, postgresVersion state.Postg
 
 		err = db.QueryRow(QueryMarkerSQL+transactionIdSQL).Scan(
 			&stats.CurrentXactId, &stats.NextMultiXactId,
+		)
+		if err != nil {
+			return stats, err
+		}
+
+		err = db.QueryRow(QueryMarkerSQL+xminHorizonSQL).Scan(
+			&stats.XminHorizonBackend, &stats.XminHorizonReplicationSlot,
+			&stats.XminHorizonPreparedXact, &stats.XminHorizonStandby,
 		)
 		if err != nil {
 			return stats, err
