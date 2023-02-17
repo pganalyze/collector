316fe85cf331db36cd217cb367dfb1cf1cbbcde8
Keiko Oda <keiko713@gmail.com>
2023-02-17 14:41:55 +0900

    Support null xmin horizon and treat them as 0

--- state/postgres_server_stats.go
+++ state/postgres_server_stats.go
@@ -1,32 +1,36 @@
 package state
 
+import (
+	"database/sql"
+)
+
 // PostgresServerStats - Statistics for a Postgres server.
 type PostgresServerStats struct {
 	CurrentXactId   Xid8
 	NextMultiXactId Xid8
 
-	XminHorizonBackend         Xid
-	XminHorizonReplicationSlot Xid
-	XminHorizonPreparedXact    Xid
-	XminHorizonStandby         Xid
+	XminHorizonBackend         sql.NullInt32
+	XminHorizonReplicationSlot sql.NullInt32
+	XminHorizonPreparedXact    sql.NullInt32
+	XminHorizonStandby         sql.NullInt32
 }
 
 // FullXminHorizonBackend - Returns XminHorizonBackend in 64-bit FullTransactionId
 func (ss PostgresServerStats) FullXminHorizonBackend() int64 {
-	return int64(XidToXid8(ss.XminHorizonBackend, Xid8(ss.CurrentXactId)))
+	return int64(XidToXid8(Xid(ss.XminHorizonBackend.Int32), Xid8(ss.CurrentXactId)))
 }
 
 // FullXminHorizonReplicationSlot - Returns XminHorizonReplicationSlot in 64-bit FullTransactionId
 func (ss PostgresServerStats) FullXminHorizonReplicationSlot() int64 {
-	return int64(XidToXid8(ss.XminHorizonReplicationSlot, Xid8(ss.CurrentXactId)))
+	return int64(XidToXid8(Xid(ss.XminHorizonReplicationSlot.Int32), Xid8(ss.CurrentXactId)))
 }
 
 // FullXminHorizonPreparedXact - Returns XminHorizonPreparedXact in 64-bit FullTransactionId
 func (ss PostgresServerStats) FullXminHorizonPreparedXact() int64 {
-	return int64(XidToXid8(ss.XminHorizonPreparedXact, Xid8(ss.CurrentXactId)))
+	return int64(XidToXid8(Xid(ss.XminHorizonPreparedXact.Int32), Xid8(ss.CurrentXactId)))
 }
 
 // FullXminHorizonStandby - Returns XminHorizonStandby in 64-bit FullTransactionId
 func (ss PostgresServerStats) FullXminHorizonStandby() int64 {
-	return int64(XidToXid8(ss.XminHorizonStandby, Xid8(ss.CurrentXactId)))
+	return int64(XidToXid8(Xid(ss.XminHorizonStandby.Int32), Xid8(ss.CurrentXactId)))
 }
