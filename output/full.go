672ea56b691f3e352462ce50874330797f25b9d8
Maciek Sakrejda <m.sakrejda@gmail.com>
2023-03-29 08:23:00 -0700

    Add integrity checks before uploading snapshots (#402)
    The snapshot format makes some structural assumptions that cannot be
    enforced by protobuf. If these are violated, processing the snapshot
    may fail, or snapshot data may be misinterpreted and can cause bogus
    statistics to be recorded.
    
    Validate these assumptions before sending a snapshot.

--- output/full.go
+++ output/full.go
@@ -26,13 +26,20 @@ import (
 func SendFull(ctx context.Context, server *state.Server, collectionOpts state.CollectionOpts, logger *util.Logger, newState state.PersistedState, diffState state.DiffState, transientState state.TransientState, collectedIntervalSecs uint32) error {
 	s := transform.StateToSnapshot(newState, diffState, transientState)
 	s.CollectedIntervalSecs = collectedIntervalSecs
-	s.CollectorErrors = logger.ErrorMessages
-
-	return submitFull(ctx, s, server, collectionOpts, logger, newState.CollectedAt, false)
+	err := verifyIntegrity(&s)
+	if err != nil {
+		logger.PrintError(fmt.Sprintf("Snapshot integrity check failed: %s; please contact support", err))
+		// Don't return an error here, since that would skip the state update, and
+		// if the integrity failure is due to a state diffing issue, we would not
+		// be able to make progress. Instead, send a failed snapshot directly.
+		return SendFailedFull(ctx, server, collectionOpts, logger)
+	} else {
+		return submitFull(ctx, s, server, collectionOpts, logger, newState.CollectedAt, false)
+	}
 }
 
 func SendFailedFull(ctx context.Context, server *state.Server, collectionOpts state.CollectionOpts, logger *util.Logger) error {
-	s := snapshot.FullSnapshot{FailedRun: true, CollectorErrors: logger.ErrorMessages}
+	s := snapshot.FullSnapshot{FailedRun: true}
 	return submitFull(ctx, s, server, collectionOpts, logger, time.Now(), true)
 }
 
@@ -42,6 +49,7 @@ func submitFull(ctx context.Context, s snapshot.FullSnapshot, server *state.Serv
 
 	snapshotUUID := uuid.NewV4()
 
+	s.CollectorErrors = logger.ErrorMessages
 	s.SnapshotVersionMajor = 1
 	s.SnapshotVersionMinor = 0
 	s.CollectorVersion = util.CollectorNameAndVersion
@@ -77,6 +85,32 @@ func submitFull(ctx context.Context, s snapshot.FullSnapshot, server *state.Serv
 	}
 
 	return submitSnapshot(ctx, server, collectionOpts, logger, s3Location, collectedAt, quiet)
+}
+
+func verifyIntegrity(s *snapshot.FullSnapshot) error {
+	if len(s.DatabaseInformations) != len(s.DatabaseReferences) {
+		return fmt.Errorf("found %d DatabaseInformations but %d DatabaseReferences", len(s.DatabaseInformations), len(s.DatabaseReferences))
+	}
+	if len(s.RoleInformations) != len(s.RoleReferences) {
+		return fmt.Errorf("found %d RoleInformations but %d RoleReferences", len(s.RoleInformations), len(s.RoleReferences))
+	}
+	if len(s.TablespaceInformations) != len(s.TablespaceReferences) {
+		return fmt.Errorf("found %d TablespaceInformations but %d TablespaceReferences", len(s.TablespaceInformations), len(s.TablespaceReferences))
+	}
+	if len(s.RelationInformations) != len(s.RelationReferences) {
+		return fmt.Errorf("found %d RelationInformations but %d RelationReferences", len(s.RelationInformations), len(s.RelationReferences))
+	}
+	if len(s.IndexInformations) != len(s.IndexReferences) {
+		return fmt.Errorf("found %d IndexInformations but %d IndexReferences", len(s.IndexInformations), len(s.IndexReferences))
+	}
+	if len(s.FunctionInformations) != len(s.FunctionReferences) {
+		return fmt.Errorf("found %d FunctionInformations but %d FunctionReferences", len(s.FunctionInformations), len(s.FunctionReferences))
+	}
+	if len(s.QueryInformations) != len(s.QueryReferences) {
+		return fmt.Errorf("found %d QueryInformations but %d QueryReferences", len(s.QueryInformations), len(s.QueryReferences))
+	}
+
+	return nil
 }
 
 func debugOutputAsJSON(logger *util.Logger, compressedData bytes.Buffer) {
