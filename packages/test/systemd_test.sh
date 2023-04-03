1289bdfc59b0199b0cc458081325211ce441f6cf
Lukas Fittl <lukas@fittl.com>
2023-04-02 19:54:53 -0700

    WIP

--- packages/test/systemd_test.sh
+++ packages/test/systemd_test.sh
@@ -9,11 +9,12 @@ systemctl status pganalyze-collector | grep -q running
 # Verify that we're running as a non-priviledged user
 ps u -U pganalyze | grep -q pganalyze-collector
 
+# Give collector time to do initial start up and be able to handle SIGHUP
+# (this can be slow when emulating a different architecture with QEMU)
+sleep 3
+
 # Verify that reloading works and emits a log notice
-sleep 3
 systemctl reload pganalyze-collector
-sleep 3
-journalctl -u pganalyze-collector -n 100
 journalctl -u pganalyze-collector | grep -q "Reloading configuration"
 
 # Verify that stopping works
