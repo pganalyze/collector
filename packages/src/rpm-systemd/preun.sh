2b40f0c6e52bfbef7bb29d155a66af0624761fa7
Lukas Fittl <lukas@fittl.com>
2023-05-12 17:47:00 -0700

    RPM packages: Ensure collector gets started after reboot
    This was an oversight in the packaging logic, since we utilize fpm's
    "--deb-systemd-auto-start" setting for the Debian-based packages, but
    we don't have the equivalent for the RPM packages.
    
    Fix by adding an explicit "systemctl enable" on installation, and a
    corresponding "systemctl disable" on removal, matching what was already
    done for the Debian packages.

--- packages/src/rpm-systemd/preun.sh
+++ packages/src/rpm-systemd/preun.sh
@@ -4,5 +4,6 @@ set -e
 
 if [ "$1" -eq "0" ]; then
   # Last version of the package is being erased
+  systemctl disable pganalyze-collector.service
   systemctl stop pganalyze-collector.service
 fi
