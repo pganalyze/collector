e36715b26a1cff3a3d4f4c531fc142957f6d7f41
Lukas Fittl <lukas@fittl.com>
2023-07-27 19:41:46 -0700

    Packages: Drop Ubuntu 16.04 and 18.04 support
    Both of these versions are now EOL, and the glibc we are building with
    (2.31 from Debian Bullseye) is newer than the glibc versions of those
    two Ubuntu releases.
    
    In passing also remove debian/jessie, which was left in by accident.

--- packages/repo/sync_deb.sh
+++ packages/repo/sync_deb.sh
@@ -5,16 +5,6 @@ set -e
 # Remove all except the master signing key, so reprepro does the right thing (subkeys are not supported)
 printf "key 1\ndelkey\ny\nkey 1\ndelkey\ny\nsave\n" | gpg --batch --command-fd 0 --edit-key $REPO_GPG_KEY
 
-mkdir -p /repo/ubuntu/xenial/conf
-cp /root/deb.distributions /repo/ubuntu/xenial/conf/distributions
-reprepro --basedir /repo/ubuntu/xenial includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
-reprepro --basedir /repo/ubuntu/xenial includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
-
-mkdir -p /repo/ubuntu/bionic/conf
-cp /root/deb.distributions /repo/ubuntu/bionic/conf/distributions
-reprepro --basedir /repo/ubuntu/bionic includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
-reprepro --basedir /repo/ubuntu/bionic includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
-
 mkdir -p /repo/ubuntu/focal/conf
 cp /root/deb.distributions /repo/ubuntu/focal/conf/distributions
 reprepro --basedir /repo/ubuntu/focal includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
@@ -24,11 +14,6 @@ mkdir -p /repo/ubuntu/jammy/conf
 cp /root/deb.distributions /repo/ubuntu/jammy/conf/distributions
 reprepro --basedir /repo/ubuntu/jammy includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
 reprepro --basedir /repo/ubuntu/jammy includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
-
-mkdir -p /repo/debian/jessie/conf
-cp /root/deb.distributions /repo/debian/jessie/conf/distributions
-reprepro --basedir /repo/debian/jessie includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
-reprepro --basedir /repo/debian/jessie includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
 
 mkdir -p /repo/debian/bullseye/conf
 cp /root/deb.distributions /repo/debian/bullseye/conf/distributions
@@ -42,10 +27,7 @@ reprepro --basedir /repo/debian/bookworm includedeb stable /deb/systemd/$DEB_PAC
 
 # Verify signatures
 apt-key add /repo/pganalyze_signing_key.asc
-gpgv --keyring /etc/apt/trusted.gpg /repo/ubuntu/xenial/dists/stable/InRelease
-gpgv --keyring /etc/apt/trusted.gpg /repo/ubuntu/bionic/dists/stable/InRelease
 gpgv --keyring /etc/apt/trusted.gpg /repo/ubuntu/focal/dists/stable/InRelease
 gpgv --keyring /etc/apt/trusted.gpg /repo/ubuntu/jammy/dists/stable/InRelease
-gpgv --keyring /etc/apt/trusted.gpg /repo/debian/jessie/dists/stable/InRelease
 gpgv --keyring /etc/apt/trusted.gpg /repo/debian/bullseye/dists/stable/InRelease
 gpgv --keyring /etc/apt/trusted.gpg /repo/debian/bookworm/dists/stable/InRelease
