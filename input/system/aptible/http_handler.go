1da9d9143f6d5c055efeaeacf4b7bbe5d3476135
Andrew Grieser <112501822+agrieser-healthie@users.noreply.github.com>
2023-11-06 17:06:38 +0000

    Debugging

--- input/system/aptible/http_handler.go
+++ input/system/aptible/http_handler.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -16,50 +15,48 @@ import (
 )
 
 type AptibleLog struct {
-	Timestamp string `json:"@timestamp"`
-	Log       string `json:"log"`
-	Host      string `json:"host"`
-	Service   string `json:"service"`
-	App       string `json:"app"`
-	AppId     string `json:"app_id"`
-	Source    string `json:"source"`
-	Container string `json:"container"`
+	Time     string `json:"time"`
+	Log      string `json:"log"`
+	Source   string `json:"source"`
+	Database string `json:"database"`
 }
 
 func SetupHttpHandlerLogs(ctx context.Context, wg *sync.WaitGroup, globalCollectionOpts state.CollectionOpts, logger *util.Logger, servers []*state.Server, parsedLogStream chan state.ParsedLogStreamItem) {
 	go func() {
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			switch r.Method {
-			case http.MethodGet:
-				w.WriteHeader(http.StatusOK)
-				w.Header().Set("Content-Type", "application/json")
-				resp := make(map[string]string)
-				resp["message"] = "Status OK"
-				jsonResp, err := json.Marshal(resp)
-				if err != nil {
-					log.Fatalf("Error happened in JSON marshal. Err: %s", err)
-				}
-				w.Write(jsonResp)
-				return
 			case http.MethodPost:
-				io.Copy(os.Stderr, r.Body)
 				decoder := json.NewDecoder(r.Body)
 
 				var logMessage AptibleLog
 				err := decoder.Decode(&logMessage)
 				if err != nil {
 					log.Fatalln("WARNING: Log message not parsed")
-				} else {
-					logLine, _ := logs.ParseLogLineWithPrefix("", logMessage.Log+"\n", nil)
-					//logLine.OccurredAt = log.Timestamp
-					//logLine.LogLineNumber = int32(logLineNumber)
-					//logLine.LogLineNumberChunk = int32(logLineNumberChunk)
-					// somehow map back to a server identifier, which is the app identifier
-					// Identifier is where it's going. LogLine is where it came from
-					//parsedLogStream <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: log.Log}
-					fmt.Println(logLine)
+					break
 				}
+				fmt.Fprintf(os.Stderr, "%+v\n", logMessage)
+
+				if (logMessage.Source != "database" || logMessage.Database != "healthie-staging-14") {
+					break
+				}
+				logLine, _ := logs.ParseLogLineWithPrefix("", logMessage.Log+"\n", nil)
+				//logLine.OccurredAt = log.Timestamp
+				//logLine.LogLineNumber = int32(logLineNumber)
+				//logLine.LogLineNumberChunk = int32(logLineNumberChunk)
+				// somehow map back to a server identifier, which is the app identifier
+				// Identifier is where it's going. LogLine is where it came from
+				//parsedLogStream <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: log.Log}
+				fmt.Fprintf(os.Stderr, "%+v\n", logLine)
 			}
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+			resp := make(map[string]string)
+			resp["message"] = "Status OK"
+			jsonResp, err := json.Marshal(resp)
+			if err != nil {
+				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+			}
+			w.Write(jsonResp)
 		})
 		http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	}()
