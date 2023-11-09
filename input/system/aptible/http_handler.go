f122d92e5dad29dcdf04ff1c412f3428b75bfa05
Andrew Grieser <112501822+agrieser-healthie@users.noreply.github.com>
2023-11-09 17:58:47 +0000

    Log the classification

--- input/system/aptible/http_handler.go
+++ input/system/aptible/http_handler.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/pganalyze/collector/logs"
-	"github.com/pganalyze/collector/output/pganalyze_collector"
 	"github.com/pganalyze/collector/state"
 	"github.com/pganalyze/collector/util"
 )
@@ -54,15 +53,14 @@ func SetupHttpHandlerLogs(ctx context.Context, wg *sync.WaitGroup, globalCollect
 				}
 				logLine, ok := logs.ParseLogLineWithPrefix("", logMessage.Log+"\n", nil)
 				if ok {
-					log.Fatalf("Log line parsed: %v\n", logLine)
 					occurredAt, err := time.Parse(time.RFC3339, logMessage.Time)
 					if err != nil {
 						log.Fatalf("Error happened time parsing. Err: %s", err)
 					}
 					logLine.OccurredAt = occurredAt
-					logger.PrintVerbose("Log line content")
-					logger.PrintVerbose(logLine.Content)
-					logLine.LogLevel = pganalyze_collector.LogLineInformation_CONTEXT
+					logger.PrintVerbose("Log line level")
+					logger.PrintVerbose(logLine.LogLevel.String())
+					//logLine.LogLevel = pganalyze_collector.LogLineInformation_CONTEXT
 					for _, server := range servers {
 						if server.Config.SectionName == "healthie-staging-14" {
 							logger.PrintVerbose("Server match, sending logs")
