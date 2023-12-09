13ba1f5aa2cab2e1000835b8dc94db6f72ed4edd
Maciek Sakrejda <m.sakrejda@gmail.com>
2023-12-08 17:25:44 -0800

    Fix Citus integration test
    The current test was running without the Citus extension, since we
    were passing stdin to `docker exec`, to pass to psql, but without
    running it in interactive mode. Docker discarded stdin, and the
    test was running without Citus-specific functionality.
    
    Pass the -i flag to `docker exec` so it relays stdin to psql.  Also
    create the Citus extension (which is not created by default in the
    Citus Docker image), and update the expected output file.

--- integration_test/citus-pgbench-ddl.sql
+++ integration_test/citus-pgbench-ddl.sql
@@ -1,3 +1,4 @@
+CREATE EXTENSION citus;
 ALTER TABLE pgbench_accounts DROP CONSTRAINT pgbench_accounts_pkey;
 ALTER TABLE pgbench_accounts ADD PRIMARY KEY (aid, bid);
 ALTER TABLE pgbench_tellers DROP CONSTRAINT pgbench_tellers_pkey;
