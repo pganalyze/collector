30fb1927b46901a738d37c3dabc7d82ec79683da
Ian Stanton <ian@tembo.io>
2024-02-28 15:42:49 -0500

    Use namespace var in URL
    Signed-off-by: Ian Stanton <ian@tembo.io>

--- input/system/tembo/system.go
+++ input/system/tembo/system.go
@@ -18,7 +18,7 @@ func GetSystemState(config config.ServerConfig, logger *util.Logger) (system sta
 	query := "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{ namespace=\"" + config.TemboNamespace + "\"}) / sum(kube_pod_container_resource_requests{job=\"kube-state-metrics\",  namespace=\"" + config.TemboNamespace + "\", resource=\"cpu\"})"
 	encodedQuery := url.QueryEscape(query)
 
-	url := "https://api.data-1.use1.cdb-dev.com/org-demo-inst-pganalyze-test/metrics/query?query=" + encodedQuery
+	url := "https://api.data-1.use1.cdb-dev.com/" + config.TemboNamespace + "/metrics/query?query=" + encodedQuery
 
 	client := http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
