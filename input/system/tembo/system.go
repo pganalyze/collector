bc9e6d1b4d311c1abdd91913faf1d1235b438ddb
Ian Stanton <ian@tembo.io>
2024-03-01 11:17:47 -0500

    DRY up functions
    Signed-off-by: Ian Stanton <ian@tembo.io>

--- input/system/tembo/system.go
+++ input/system/tembo/system.go
@@ -47,7 +47,8 @@ func GetSystemState(config config.ServerConfig, logger *util.Logger) (system sta
 	client := http.Client{}
 
 	// Get CPU usage percentage
-	cpuUsage, err := getCpuUsage("https://api.data-1.use1.cdb-dev.com/"+config.TemboNamespace+"/metrics/query?query=", client, headers, config.TemboNamespace, logger)
+	query := "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{ namespace=\"" + config.TemboNamespace + "\"}) / sum(kube_pod_container_resource_requests{job=\"kube-state-metrics\",  namespace=\"" + config.TemboNamespace + "\", resource=\"cpu\"})"
+	cpuUsage, err := getFloat64(query, "https://api.data-1.use1.tembo.io/"+config.TemboNamespace+"/metrics/query?query=", client, headers)
 	if err != nil {
 		logger.PrintError("Tembo/System: Encountered error when getting CPU info %v\n", err)
 		return
@@ -62,14 +63,16 @@ func GetSystemState(config config.ServerConfig, logger *util.Logger) (system sta
 	}
 
 	// Get total memory
-	memoryTotalBytes, err := getTotalMemory("https://api.data-1.use1.cdb-dev.com/"+config.TemboNamespace+"/metrics/query?query=", client, headers, config.TemboNamespace, logger)
+	query = "sum(max by(pod) (kube_pod_container_resource_requests{job=\"kube-state-metrics\", namespace=\"" + config.TemboNamespace + "\", resource=\"memory\"}))"
+	memoryTotalBytes, err := getUint64(query, "https://api.data-1.use1.tembo.io/"+config.TemboNamespace+"/metrics/query?query=", client, headers)
 	if err != nil {
 		logger.PrintError("Tembo/System: Encountered error when getting memory info %v\n", err)
 		return
 	}
 
 	// Get available memory
-	memoryAvailableBytes, err := getAvailableMemory("https://api.data-1.use1.cdb-dev.com/"+config.TemboNamespace+"/metrics/query?query=", client, headers, config.TemboNamespace, logger)
+	query = "sum(max by(pod) (kube_pod_container_resource_requests{job=\"kube-state-metrics\", namespace=\"" + config.TemboNamespace + "\", resource=\"memory\"})) - sum(container_memory_working_set_bytes{job=\"kubelet\", metrics_path=\"/metrics/cadvisor\", namespace=\"" + config.TemboNamespace + "\",container!=\"\", image!=\"\"})"
+	memoryAvailableBytes, err := getUint64(query, "https://api.data-1.use1.tembo.io/"+config.TemboNamespace+"/metrics/query?query=", client, headers)
 	if err != nil {
 		logger.PrintError("Tembo/System: Encountered error when getting memory info %v\n", err)
 		return
@@ -79,13 +82,19 @@ func GetSystemState(config config.ServerConfig, logger *util.Logger) (system sta
 	system.Memory.AvailableBytes = memoryAvailableBytes
 	system.Memory.FreeBytes = memoryAvailableBytes
 
-	diskCapacity, err := getDiskCapacity("https://api.data-1.use1.cdb-dev.com/"+config.TemboNamespace+"/metrics/query?query=", client, headers, config.TemboNamespace, logger)
+	// Get disk capacity
+	//TODO(ianstanton) Check if volume claim names differ in cases like HA
+	query = "kubelet_volume_stats_capacity_bytes{namespace=\"" + config.TemboNamespace + "\", persistentvolumeclaim=~\"" + config.TemboNamespace + "-1" + "\"}"
+	diskCapacity, err := getUint64(query, "https://api.data-1.use1.tembo.io/"+config.TemboNamespace+"/metrics/query?query=", client, headers)
 	if err != nil {
 		logger.PrintError("Tembo/System: Encountered error when getting disk info %v\n", err)
 		return
 	}
 
-	diskAvailable, err := getDiskAvailable("https://api.data-1.use1.cdb-dev.com/"+config.TemboNamespace+"/metrics/query?query=", client, headers, config.TemboNamespace, logger)
+	// Get disk available
+	//TODO(ianstanton) Check if volume claim names differ in cases like HA
+	query = "kubelet_volume_stats_available_bytes{namespace=\"" + config.TemboNamespace + "\", persistentvolumeclaim=~\"" + config.TemboNamespace + "-1" + "\"}"
+	diskAvailable, err := getUint64(query, "https://api.data-1.use1.tembo.io/"+config.TemboNamespace+"/metrics/query?query=", client, headers)
 	if err != nil {
 		logger.PrintError("Tembo/System: Encountered error when getting disk info %v\n", err)
 		return
@@ -103,57 +112,31 @@ func GetSystemState(config config.ServerConfig, logger *util.Logger) (system sta
 	return
 }
 
-func getCpuUsage(metricsUrl string, client http.Client, headers map[string]string, namespace string, logger *util.Logger) (float64, error) {
-	query := "sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{ namespace=\"" + namespace + "\"}) / sum(kube_pod_container_resource_requests{job=\"kube-state-metrics\",  namespace=\"" + namespace + "\", resource=\"cpu\"})"
-	res, err := getSystemInfo(metricsUrl, query, client, headers, logger)
+func getFloat64(query string, metricsUrl string, client http.Client, headers map[string]string) (float64, error) {
+	res, err := getSystemInfo(metricsUrl, query, client, headers)
 	if err != nil {
 		return 0, err
 	}
 
-	// Get cpuUsage from response
-	cpuUsageStr := res.Data.Result[0].Value[1].(string)
+	strValue := res.Data.Result[0].Value[1].(string)
+	value, err := strconv.ParseFloat(strValue, 64)
 
-	// Convert cpuUsage to float64
-	cpuUsage, err := strconv.ParseFloat(cpuUsageStr, 64)
-
-	return cpuUsage, nil
+	return value, nil
 }
 
-func getTotalMemory(metricsUrl string, client http.Client, headers map[string]string, namespace string, logger *util.Logger) (uint64, error) {
-	query := "sum(max by(pod) (kube_pod_container_resource_requests{job=\"kube-state-metrics\", namespace=\"" + namespace + "\", resource=\"memory\"}))"
-
-	res, err := getSystemInfo(metricsUrl, query, client, headers, logger)
+func getUint64(query string, metricsUrl string, client http.Client, headers map[string]string) (uint64, error) {
+	res, err := getSystemInfo(metricsUrl, query, client, headers)
 	if err != nil {
 		return 0, err
 	}
 
-	// Get totalMemory from response
-	totalMemoryStr := res.Data.Result[0].Value[1].(string)
+	strValue := res.Data.Result[0].Value[1].(string)
+	value, err := strconv.ParseUint(strValue, 10, 64)
 
-	// Convert totalMemory to uint64
-	totalMemory, err := strconv.ParseUint(totalMemoryStr, 10, 64)
-
-	return totalMemory, nil
+	return value, nil
 }
 
-func getAvailableMemory(metricsUrl string, client http.Client, headers map[string]string, namespace string, logger *util.Logger) (uint64, error) {
-	query := "sum(max by(pod) (kube_pod_container_resource_requests{job=\"kube-state-metrics\", namespace=\"" + namespace + "\", resource=\"memory\"})) - sum(container_memory_working_set_bytes{job=\"kubelet\", metrics_path=\"/metrics/cadvisor\", namespace=\"" + namespace + "\",container!=\"\", image!=\"\"})"
-
-	res, err := getSystemInfo(metricsUrl, query, client, headers, logger)
-	if err != nil {
-		return 0, err
-	}
-
-	// Get availableMemory from response
-	availableMemoryStr := res.Data.Result[0].Value[1].(string)
-
-	// Convert availableMemory to uint64
-	availableMemory, err := strconv.ParseUint(availableMemoryStr, 10, 64)
-
-	return availableMemory, nil
-}
-
-func getSystemInfo(metricsUrl string, query string, client http.Client, headers map[string]string, logger *util.Logger) (Response, error) {
+func getSystemInfo(metricsUrl string, query string, client http.Client, headers map[string]string) (Response, error) {
 	encodedQuery := url.QueryEscape(query)
 
 	metricsUrl = metricsUrl + encodedQuery
@@ -179,40 +162,4 @@ func getSystemInfo(metricsUrl string, query string, client http.Client, headers 
 	}
 
 	return result, nil
-}
-
-func getDiskCapacity(metricsUrl string, client http.Client, headers map[string]string, namespace string, logger *util.Logger) (uint64, error) {
-	//TODO(ianstanton) Check if volume claim names differ in cases like HA
-	query := "kubelet_volume_stats_capacity_bytes{namespace=\"" + namespace + "\", persistentvolumeclaim=~\"" + namespace + "-1" + "\"}"
-
-	res, err := getSystemInfo(metricsUrl, query, client, headers, logger)
-	if err != nil {
-		return 0, err
-	}
-
-	// Get availableDisk from response
-	availableDiskStr := res.Data.Result[0].Value[1].(string)
-
-	// Convert availableDisk to uint64
-	availableDisk, err := strconv.ParseUint(availableDiskStr, 10, 64)
-
-	return availableDisk, nil
-}
-
-func getDiskAvailable(metricsUrl string, client http.Client, headers map[string]string, namespace string, logger *util.Logger) (uint64, error) {
-	//TODO(ianstanton) Check if volume claim names differ in cases like HA
-	query := "kubelet_volume_stats_available_bytes{namespace=\"" + namespace + "\", persistentvolumeclaim=~\"" + namespace + "-1" + "\"}"
-
-	res, err := getSystemInfo(metricsUrl, query, client, headers, logger)
-	if err != nil {
-		return 0, err
-	}
-
-	// Get usedDisk from response
-	usedDiskStr := res.Data.Result[0].Value[1].(string)
-
-	// Convert usedDisk to uint64
-	usedDisk, err := strconv.ParseUint(usedDiskStr, 10, 64)
-
-	return usedDisk, nil
 }
