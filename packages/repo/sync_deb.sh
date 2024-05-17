13ea63e20292310fd4082d6ccdbba613ec9f4f63
Maciek Sakrejda <m.sakrejda@gmail.com>
2024-05-17 14:44:55 -0700

    Add Ubuntu 24.04

--- packages/repo/sync_deb.sh
+++ packages/repo/sync_deb.sh
@@ -15,6 +15,11 @@ cp /root/deb.distributions /repo/ubuntu/jammy/conf/distributions
 reprepro --basedir /repo/ubuntu/jammy includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
 reprepro --basedir /repo/ubuntu/jammy includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
 
+mkdir -p /repo/ubuntu/noble/conf
+cp /root/deb.distributions /repo/ubuntu/noble/conf/distributions
+reprepro --basedir /repo/ubuntu/noble includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
+reprepro --basedir /repo/ubuntu/noble includedeb stable /deb/systemd/$DEB_PACKAGE_ARM64
+
 mkdir -p /repo/debian/bullseye/conf
 cp /root/deb.distributions /repo/debian/bullseye/conf/distributions
 reprepro --basedir /repo/debian/bullseye includedeb stable /deb/systemd/$DEB_PACKAGE_X86_64
@@ -29,5 +34,6 @@ reprepro --basedir /repo/debian/bookworm includedeb stable /deb/systemd/$DEB_PAC
 apt-key add /repo/pganalyze_signing_key.asc
 gpgv --keyring /etc/apt/trusted.gpg /repo/ubuntu/focal/dists/stable/InRelease
 gpgv --keyring /etc/apt/trusted.gpg /repo/ubuntu/jammy/dists/stable/InRelease
+gpgv --keyring /etc/apt/trusted.gpg /repo/ubuntu/noble/dists/stable/InRelease
 gpgv --keyring /etc/apt/trusted.gpg /repo/debian/bullseye/dists/stable/InRelease
 gpgv --keyring /etc/apt/trusted.gpg /repo/debian/bookworm/dists/stable/InRelease
