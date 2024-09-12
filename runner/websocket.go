2e019f4bd78490463ce9610c3371c8ec9f93a3aa
Sean Linsley <code@seanlinsley.com>
2024-09-12 16:31:26 -0500

    Add proxy support
    In passing, increase the default websocket buffer size from 4KB to 10KB

--- runner/websocket.go
+++ runner/websocket.go
@@ -5,6 +5,7 @@ import (
 	"compress/zlib"
 	"context"
 	"io"
+	"net/http"
 	"net/url"
 	"time"
 
@@ -12,6 +13,7 @@ import (
 	"github.com/pganalyze/collector/output/pganalyze_collector"
 	"github.com/pganalyze/collector/state"
 	"github.com/pganalyze/collector/util"
+	"golang.org/x/net/http/httpproxy"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -31,6 +33,18 @@ func SetupWebsocketForAllServers(ctx context.Context, servers []*state.Server, g
 
 func connect(ctx context.Context, server *state.Server, globalCollectionOpts state.CollectionOpts, logger *util.Logger) {
 	connCtx, cancelConn := context.WithCancel(ctx)
+	proxyConfig := httpproxy.Config{
+		HTTPProxy:  server.Config.HTTPProxy,
+		HTTPSProxy: server.Config.HTTPSProxy,
+		NoProxy:    server.Config.NoProxy,
+	}
+	dialer := websocket.Dialer{
+		ReadBufferSize:  10240,
+		WriteBufferSize: 10240,
+		Proxy: func(req *http.Request) (*url.URL, error) {
+			return proxyConfig.ProxyFunc()(req.URL)
+		},
+	}
 	url, _ := url.Parse(server.Config.APIBaseURL + "/v2/snapshots/websocket")
 	if url.Scheme == "http" {
 		url.Scheme = "ws"
@@ -46,7 +60,7 @@ func connect(ctx context.Context, server *state.Server, globalCollectionOpts sta
 	headers["Pganalyze-System-Type-Fallback"] = []string{server.Config.SystemTypeFallback}
 	headers["Pganalyze-System-Scope-Fallback"] = []string{server.Config.SystemScopeFallback}
 	headers["User-Agent"] = []string{util.CollectorNameAndVersion}
-	conn, response, err := websocket.DefaultDialer.DialContext(connCtx, url.String(), headers)
+	conn, response, err := dialer.DialContext(connCtx, url.String(), headers)
 	if err != nil {
 		cancelConn()
 		logger.PrintWarning("Error starting websocket: %s %v", err, response)
@@ -54,6 +68,7 @@ func connect(ctx context.Context, server *state.Server, globalCollectionOpts sta
 	}
 	server.WebSocket.Store(conn)
 	server.Pause.Store(&pganalyze_collector.ServerMessage_Pause{Pause: false})
+	// Writer goroutine
 	go func() {
 		for {
 			select {
@@ -77,6 +92,7 @@ func connect(ctx context.Context, server *state.Server, globalCollectionOpts sta
 			}
 		}
 	}()
+	// Reader goroutine
 	go func() {
 		for {
 			_, compressedData, err := conn.ReadMessage()
