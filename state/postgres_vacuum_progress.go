692d7861b3eeaa38f602359b4e219445f5818a89
Lukas Fittl <lukas@fittl.com>
2024-10-01 14:37:20 -0400

    Track all additional fields in pg_stat_progress_vacuum in PG17+
    In passing, this also renames max_dead_tuples and num_dead_tuples
    to max_dead_item_ids/num_dead_item_ids, to track the upstream
    clarification on the naming.

--- state/postgres_vacuum_progress.go
+++ state/postgres_vacuum_progress.go
@@ -4,7 +4,7 @@ import "time"
 
 // PostgresVacuumProgress - PostgreSQL vacuum thats currently running
 //
-// See https://www.postgresql.org/docs/10/static/progress-reporting.html
+// See https://www.postgresql.org/docs/current/progress-reporting.html
 type PostgresVacuumProgress struct {
 	VacuumIdentity  uint64 // Combination of vacuum "query" start time and PID, used to identify a vacuum over time
 	BackendIdentity uint64 // Combination of process start time and PID, used to identify a process over time
@@ -17,11 +17,15 @@ type PostgresVacuumProgress struct {
 	Autovacuum   bool
 	Toast        bool
 
-	Phase            string
-	HeapBlksTotal    int64
-	HeapBlksScanned  int64
-	HeapBlksVacuumed int64
-	IndexVacuumCount int64
-	MaxDeadTuples    int64
-	NumDeadTuples    int64
+	Phase             string
+	HeapBlksTotal     int64
+	HeapBlksScanned   int64
+	HeapBlksVacuumed  int64
+	IndexVacuumCount  int64
+	MaxDeadItemIds    int64
+	NumDeadItemIds    int64
+	DeadTupleBytes    int64
+	MaxDeadTupleBytes int64
+	IndexesTotal      int64
+	IndexesProcessed  int64
 }
