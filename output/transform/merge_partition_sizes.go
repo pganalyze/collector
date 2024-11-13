bf441ccd288acf3801ddaedad19c409846ca6597
Sean Linsley <code@seanlinsley.com>
2024-11-13 16:58:45 -0600

    Aggregate stats for partition tables

---
+++ output/transform/merge_partition_sizes.go
@@ -0,0 +1,56 @@
+package transform
+
+import (
+	snapshot "github.com/pganalyze/collector/output/pganalyze_collector"
+	"github.com/pganalyze/collector/state"
+)
+
+func mergePartitionSizes(s snapshot.FullSnapshot, newState state.PersistedState, ts state.TransientState, databaseOidToIdx OidToIdx) snapshot.FullSnapshot {
+	for idx, info := range s.IndexInformations {
+		rel := s.RelationInformations[info.RelationIdx]
+		if !rel.HasParentRelation || rel.PartitionBoundary == "" {
+			continue
+		}
+		for parentIdx, pi := range s.IndexInformations {
+			if pi.RelationIdx != rel.ParentRelationIdx {
+				continue
+			}
+			if info.IndexType == pi.IndexType && info.IsUnique == pi.IsUnique && intArrayEqual(info.Columns, pi.Columns) {
+				s.IndexStatistics[parentIdx].SizeBytes += s.IndexStatistics[idx].SizeBytes
+				break
+			}
+		}
+	}
+
+	for idx, rel := range s.RelationInformations {
+		if !rel.HasParentRelation || rel.PartitionBoundary == "" {
+			continue
+		}
+		stat := s.RelationStatistics[idx]
+		parent := s.RelationStatistics[rel.ParentRelationIdx]
+		parent.NTupIns += stat.NTupIns
+		parent.NTupUpd += stat.NTupUpd
+		parent.NTupDel += stat.NTupDel
+		parent.NTupHotUpd += stat.NTupHotUpd
+		parent.NLiveTup += stat.NLiveTup
+		parent.NDeadTup += stat.NDeadTup
+		parent.SizeBytes += stat.SizeBytes
+		parent.ToastSizeBytes += stat.ToastSizeBytes
+		parent.CachedDataBytes += stat.CachedDataBytes
+		parent.CachedToastBytes += stat.CachedToastBytes
+	}
+
+	return s
+}
+
+func intArrayEqual(a []int32, b []int32) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
