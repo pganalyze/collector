a32fd47e7ab4042fa9c280567f63f2ce1b97c0d5
Keiko Oda <keiko713@gmail.com>
2024-12-04 14:50:09 +0900

    Azure Metrics: fix getFirstMetricValue to return the first value (#648)
    * Make getFirstMetricValue to return the first value
    
    This function was previously returning the last value instead of the
    first value. This fix changes it and returns the value as soon as
    reading it.
    
    Co-authored-by: Maciek Sakrejda <m.sakrejda@gmail.com>

--- input/system/azure/system.go
+++ input/system/azure/system.go
@@ -283,10 +283,8 @@ func GetSystemState(ctx context.Context, server *state.Server, logger *util.Logg
 
 // getFirstMetricValue gets the first data from the time series metric and returns the value
 func getFirstMetricValue(metric *azquery.Metric) (metricValue *azquery.MetricValue) {
-	for _, timeSeriesElement := range metric.TimeSeries {
-		for _, mValue := range timeSeriesElement.Data {
-			metricValue = mValue
-		}
+	if len(metric.TimeSeries) == 0 || len(metric.TimeSeries[0].Data) == 0 {
+		return
 	}
-	return
+	return metric.TimeSeries[0].Data[0]
 }
