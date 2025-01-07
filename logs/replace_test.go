7499212028156b92ee0325319cafc33a45b2ab01
Lukas Fittl <lukas@fittl.com>
2025-01-07 12:09:47 -0800

    PII Filtering: Detect bind parameters in CONTEXT as statement_text
    This can occur in certain cases, for example auto_explain output
    on some Postgres versions. We previously would have not detected
    this CONTEXT line, but are now correctly detecting it as containing
    statement_parameter log secrets.
    
    In passing add a test case for the new "Query Parameters" field in
    auto_explain which gets redacted based on the statement_text filter that
    filters out the whole plan text (we normalize the individual fields in
    resulting query samples, but the log text for auto_explain is redacted
    altogether).

--- logs/replace_test.go
+++ logs/replace_test.go
@@ -30,9 +30,19 @@ var replaceTests = []replaceTestpair{
 		output:          "duration: 1242.570 ms  statement: [redacted]\n",
 	},
 	{
+		filterLogSecret: "statement_text",
+		input:           "2018-03-11 20:00:02 UTC:1.1.1.1(2):a@b:[3]:LOG:  duration: 2007.111 ms  plan:\n{\"Query Text\": \"SELECT pg_sleep($1)\", \"Query Parameters\": \"$1 = '2'\", \"Plan\": { } }\n",
+		output:          "duration: 2007.111 ms  plan:\n[redacted]\n",
+	},
+	{
 		filterLogSecret: "statement_parameter",
 		input:           "2018-03-11 20:00:02 UTC:1.1.1.1(2):a@b:[3]:LOG:  duration: 4079.697 ms  execute <unnamed>: \nSELECT * FROM x WHERE y = $1 LIMIT $2\n2018-03-11 20:00:02 UTC:1.1.1.1(2):a@b:[3]:DETAIL:  parameters: $1 = 'long string', $2 = '1', $3 = 'long string'\n",
 		output:          "duration: 4079.697 ms  execute <unnamed>: \nSELECT * FROM x WHERE y = $1 LIMIT $2\nparameters: $1 = '[redacted]', $2 = '[redacted]', $3 = '[redacted]'\n",
+	},
+	{
+		filterLogSecret: "statement_parameter",
+		input:           "2018-03-11 20:00:02 UTC:1.1.1.1(2):a@b:[3]:LOG:  duration: 2007.111 ms  plan:\n{\"Query Text\": \"SELECT * FROM x WHERE y = $1 LIMIT $2\", \"Plan\": { } }\n2018-03-11 20:00:02 UTC:1.1.1.1(2):a@b:[3]:CONTEXT: unnamed portal with parameters: $1 = 'long string', $2 = '1', $3 = 'long string'\n",
+		output:          "duration: 2007.111 ms  plan:\n{\"Query Text\": \"SELECT * FROM x WHERE y = $1 LIMIT $2\", \"Plan\": { } }\nunnamed portal with parameters: $1 = '[redacted]', $2 = '[redacted]', $3 = '[redacted]'\n",
 	},
 	{
 		filterLogSecret: "none",
