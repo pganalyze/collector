pganalyze/collector

commit 768c8019ab80d9596326b8e976b7371c7f91fc45

Add alternate mechanism for parsing log lines (#494)
Instead of supporting a list of predefined `log_line_prefix`es, and testing
each log line against each prefix, check `log_line_prefix` at startup (and
watch for changes during full snapshots), and build a custom regex based
on the prefix. This is faster and more flexible, since it can support arbitrary
prefixes.

Add a `db_log_line_prefix` option for now to fall back to the old behavior
in case there are issues with the new mechanism. The old parsing code
and this option can be dropped in a future patch.

input/system/heroku/logs.go
@@ -119,6 +119,8 @@ func processSystemMetrics(ctx context.Context, timestamp time.Time, content []by
 	}
 }
 
+var HerokuLogParser = logs.NewLogParser(logs.LogPrefixHeroku2, nil, false, true)
+
 func logStreamItemToLogLine(ctx context.Context, item HttpSyslogMessage, servers []*state.Server, sourceToServer map[string]*state.Server, now time.Time, globalCollectionOpts state.CollectionOpts, logger *util.Logger) (map[string]*state.Server, *state.LogLine, string) {
 	timestamp, err := time.Parse(time.RFC3339, item.HeaderTimestamp)
 	if err != nil {
@@ -156,7 +158,7 @@ func logStreamItemToLogLine(ctx context.Context, item HttpSyslogMessage, servers
 	logLineNumberChunk, _ := strconv.ParseInt(lineParts[3], 10, 32)
 	prefixedContent := lineParts[4]
 
-	logLine, _ := logs.ParseLogLineWithPrefix("", prefixedContent+"\n", nil)
+	logLine, _ := HerokuLogParser.ParseLine(prefixedContent + "\n")
 
 	sourceToServer = catchIdentifyServerLine(sourceName, logLine.Content, sourceToServer, servers)
 

main.go
@@ -619,9 +619,11 @@ func checkOneInitialCollectionStatus(ctx context.Context, server *state.Server, 
 		logger.PrintInfo("Log statement lines will be ignored for this server: %s", logsDisabledReason)
 	}
 
-	server.SetLogTimezone(settings)
-	if server.LogTimezone == nil {
-		logger.PrintWarning("Could not determine log timezone for this server: %s")
+	logs.SyncLogParser(server, settings)
+	parser := server.GetLogParser()
+	prefixErr := parser.ValidatePrefix()
+	if prefixErr != nil {
+		logger.PrintWarning("Checking log_line_prefix: %s", prefixErr)
 	}
 
 	server.CollectionStatusMutex.Lock()

state/state.go
@@ -278,10 +278,11 @@ type Server struct {
 
 	SelfTest *SelfTestResult
 
-	// The time zone that logs are parsed in, synced from the setting log_timezone
-	// The StateMutex should be held while updating this
-	LogTimezone      *time.Location
-	LogTimezoneMutex *sync.Mutex
+	// The LogParser for this server, updated as necessary whenever relevant
+	// settings (log_line_prefix and log_timezone) change
+	// The LogSettingsMutex should be held while updating this
+	LogParser     LogParser
+	LogParseMutex *sync.RWMutex
 
 	// Boolean flags for which log lines should be ignored for processing
 	//
@@ -302,7 +303,7 @@ func MakeServer(config config.ServerConfig, testRun bool) *Server {
 		LogStateMutex:         &sync.Mutex{},
 		ActivityStateMutex:    &sync.Mutex{},
 		CollectionStatusMutex: &sync.Mutex{},
-		LogTimezoneMutex:      &sync.Mutex{},
+		LogParseMutex:         &sync.RWMutex{},
 	}
 	if testRun {
 		server.SelfTest = MakeSelfTest()
@@ -315,6 +316,13 @@ const (
 	LOG_IGNORE_DURATION
 )
 
+type LogParser interface {
+	Matches(prefix string, tz *time.Location, isSyslog bool) bool
+	GetOccurredAt(timePart string) time.Time
+	ParseLine(line string) (logLine LogLine, ok bool)
+	ValidatePrefix() error
+}
+
 func (s *Server) SetLogIgnoreFlags(ignoreStatement bool, ignoreDuration bool) {
 	var newFlags uint32
 	if ignoreStatement {
@@ -326,22 +334,11 @@ func (s *Server) SetLogIgnoreFlags(ignoreStatement bool, ignoreDuration bool) {
 	atomic.StoreUint32(&s.LogIgnoreFlags, newFlags)
 }
 
-func (s *Server) SetLogTimezone(settings []PostgresSetting) {
-	tz := getTimeZoneFromSettings(settings)
+func (s *Server) GetLogParser() LogParser {
+	s.LogParseMutex.RLock()
+	defer s.LogParseMutex.RUnlock()
 
-	s.LogTimezoneMutex.Lock()
-	defer s.LogTimezoneMutex.Unlock()
-	s.LogTimezone = tz
-}
-
-func (s *Server) GetLogTimezone() *time.Location {
-	s.LogTimezoneMutex.Lock()
-	defer s.LogTimezoneMutex.Unlock()
-	if s.LogTimezone == nil {
-		return nil
-	}
-	tz := *s.LogTimezone
-	return &tz
+	return s.LogParser
 }
 
 // IgnoreLogLine - helper function that lets callers determine whether a log

input/system/azure/logs_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/kylelemons/godebug/pretty"
 	"github.com/pganalyze/collector/input/system/azure"
+	"github.com/pganalyze/collector/logs"
 	"github.com/pganalyze/collector/output/pganalyze_collector"
 	"github.com/pganalyze/collector/state"
 )
@@ -277,24 +278,43 @@ var parseRecordTests = []parseRecordTestpair{
 }
 
 func TestParseRecordToLogLines(t *testing.T) {
-	for _, pair := range parseRecordTests {
+	for i, pair := range parseRecordTests {
+		var prefix string
+		if i < 3 {
+			prefix = logs.LogPrefixCustom3
+		} else {
+			prefix = logs.LogPrefixAzure
+		}
+		parser := logs.NewLogParser(prefix, nil, false, false)
+
 		var record azure.AzurePostgresLogRecord
 		err := json.Unmarshal([]byte(pair.recordIn), &record)
 		if err != nil {
 			t.Errorf("For \"%v\": expected unmarshaling to succeed, but it failed: %s\n", pair.recordIn, err)
 		}
-		lines, serverName, err := azure.ParseRecordToLogLines(record)
+		lines, err := azure.ParseRecordToLogLines(record, parser)
 		if pair.errOut != err {
 			t.Errorf("For \"%v\": expected error to be %v, but was %v\n", pair.recordIn, pair.errOut, err)
-		}
-		if pair.serverNameOut != serverName {
-			t.Errorf("For \"%v\": expected server name to be %v, but was %v\n", pair.recordIn, pair.serverNameOut, serverName)
 		}
 
 		cfg := pretty.CompareConfig
 		cfg.SkipZeroFields = true
 		if diff := cfg.Compare(lines, pair.linesOut); diff != "" {
 			t.Errorf("For \"%v\": log line diff: (-got +want)\n%s", pair.recordIn, diff)
+		}
+	}
+}
+
+func TestGetServerNameFromRecord(t *testing.T) {
+	for _, pair := range parseRecordTests {
+		var record azure.AzurePostgresLogRecord
+		err := json.Unmarshal([]byte(pair.recordIn), &record)
+		if err != nil {
+			t.Errorf("For \"%v\": expected unmarshaling to succeed, but it failed: %s\n", pair.recordIn, err)
+		}
+		serverName := azure.GetServerNameFromRecord(record)
+		if pair.serverNameOut != serverName {
+			t.Errorf("For \"%v\": expected server name to be %v, but was %v\n", pair.recordIn, pair.serverNameOut, serverName)
 		}
 	}
 }

logs/replace_test.go
@@ -64,7 +64,8 @@ var replaceTests = []replaceTestpair{
 func TestReplaceSecrets(t *testing.T) {
 	for _, pair := range replaceTests {
 		reader := bufio.NewReader(strings.NewReader(pair.input))
-		logLines, _ := logs.ParseAndAnalyzeBuffer(reader, time.Time{}, &state.Server{LogTimezoneMutex: &sync.Mutex{}})
+		server := &state.Server{LogParseMutex: &sync.RWMutex{}, LogParser: logs.NewLogParser(logs.LogPrefixAmazonRds, nil, false, false)}
+		logLines, _ := logs.ParseAndAnalyzeBuffer(reader, time.Time{}, server)
 		output := logs.ReplaceSecrets([]byte(pair.input), logLines, state.ParseFilterLogSecret(pair.filterLogSecret))
 
 		cfg := pretty.CompareConfig

input/system/azure/logs.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/pganalyze/collector/config"
-	"github.com/pganalyze/collector/logs"
 	"github.com/pganalyze/collector/output/pganalyze_collector"
 	"github.com/pganalyze/collector/state"
 	"github.com/pganalyze/collector/util"
@@ -229,17 +228,21 @@ func SetupLogSubscriber(ctx context.Context, wg *sync.WaitGroup, globalCollectio
 	return nil
 }
 
-// Parses one Azure Event Hub log record into one or two log lines (main + DETAIL)
-func ParseRecordToLogLines(in AzurePostgresLogRecord) ([]state.LogLine, string, error) {
-	var azureDbServerName string
-
-	logLineContent := in.Properties.Message
-
+func GetServerNameFromRecord(in AzurePostgresLogRecord) string {
 	if in.LogicalServerName == "" { // Flexible Server
 		// For Flexible Server, logical server name is not set, so instead determine it based on the resource ID
 		resourceParts := strings.Split(in.ResourceID, "/")
-		azureDbServerName = strings.ToLower(resourceParts[len(resourceParts)-1])
+		return strings.ToLower(resourceParts[len(resourceParts)-1])
 	} else { // Single Server
+		return in.LogicalServerName
+	}
+}
+
+// Parses one Azure Event Hub log record into one or two log lines (main + DETAIL)
+func ParseRecordToLogLines(in AzurePostgresLogRecord, parser state.LogParser) ([]state.LogLine, error) {
+	logLineContent := in.Properties.Message
+
+	if in.LogicalServerName != "" { // Single Server
 		// Adjust Azure-modified log messages to be standard Postgres log messages
 		if strings.HasPrefix(logLineContent, "connection received:") {
 			logLineContent = connectionReceivedRegexp.ReplaceAllString(logLineContent, "$1")
@@ -253,13 +256,10 @@ func ParseRecordToLogLines(in AzurePostgresLogRecord) ([]state.LogLine, string, 
 		// Add prefix and error level, which are separated from the content on
 		// Single Server (but our parser expects them together)
 		logLineContent = fmt.Sprintf("%s%s:  %s", in.Properties.Prefix, in.Properties.ErrorLevel, logLineContent)
-
-		azureDbServerName = in.LogicalServerName
 	}
-
-	logLine, ok := logs.ParseLogLineWithPrefix("", logLineContent, nil)
+	logLine, ok := parser.ParseLine(logLineContent)
 	if !ok {
-		return []state.LogLine{}, "", fmt.Errorf("Can't parse log line: \"%s\"", logLineContent)
+		return []state.LogLine{}, fmt.Errorf("Can't parse log line: \"%s\"", logLineContent)
 	}
 
 	logLines := []state.LogLine{logLine}
@@ -275,7 +275,7 @@ func ParseRecordToLogLines(in AzurePostgresLogRecord) ([]state.LogLine, string, 
 		logLines = append(logLines, detailLogLine)
 	}
 
-	return logLines, azureDbServerName, nil
+	return logLines, nil
 }
 
 func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*state.Server, in <-chan AzurePostgresLogRecord, out chan state.ParsedLogStreamItem, globalCollectionOpts state.CollectionOpts, logger *util.Logger) {
@@ -296,7 +296,21 @@ func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*sta
 					return
 				}
 
-				logLines, azureDbServerName, err := ParseRecordToLogLines(in)
+				azureDbServerName := GetServerNameFromRecord(in)
+				var server *state.Server
+				for _, s := range servers {
+					if azureDbServerName == s.Config.AzureDbServerName {
+						server = s
+					}
+				}
+				if server == nil {
+					if globalCollectionOpts.TestRun {
+						logger.PrintVerbose("Discarding log line because of unknown server (did you set the correct azure_db_server_name?): %s", azureDbServerName)
+					}
+					continue
+				}
+				parser := server.GetLogParser()
+				logLines, err := ParseRecordToLogLines(in, parser)
 				if err != nil {
 					logger.PrintError("%s", err)
 					continue
@@ -310,19 +324,8 @@ func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*sta
 					continue
 				}
 
-				foundServer := false
-				for _, server := range servers {
-					if azureDbServerName == server.Config.AzureDbServerName {
-						foundServer = true
-
-						for _, logLine := range logLines {
-							out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
-						}
-					}
-				}
-
-				if !foundServer && globalCollectionOpts.TestRun {
-					logger.PrintVerbose("Discarding log line because of unknown server (did you set the correct azure_db_server_name?): %s", azureDbServerName)
+				for _, logLine := range logLines {
+					out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
 				}
 			}
 		}

input/system/google_cloudsql/logs.go
@@ -13,7 +13,6 @@ import (
 	"google.golang.org/api/option"
 
 	"github.com/pganalyze/collector/config"
-	"github.com/pganalyze/collector/logs"
 	"github.com/pganalyze/collector/state"
 	"github.com/pganalyze/collector/util"
 )
@@ -187,12 +186,31 @@ func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*sta
 					return
 				}
 
+				var server *state.Server
+				var isAlloyDBCluster bool
+
+				for _, s := range servers {
+					if in.GcpProjectID == s.Config.GcpProjectID && in.GcpCloudSQLInstanceID != "" && in.GcpCloudSQLInstanceID == s.Config.GcpCloudSQLInstanceID {
+						server = s
+					}
+					if in.GcpProjectID == s.Config.GcpProjectID && in.GcpAlloyDBClusterID != "" && in.GcpAlloyDBClusterID == s.Config.GcpAlloyDBClusterID && in.GcpAlloyDBInstanceID != "" && in.GcpAlloyDBInstanceID == s.Config.GcpAlloyDBInstanceID {
+						server = s
+						isAlloyDBCluster = true
+					}
+				}
+
+				if server == nil {
+					continue
+				}
+
+				parser := server.GetLogParser()
+
 				// We ignore failures here since we want the per-backend stitching logic
 				// that runs later on (and any other parsing errors will just be ignored).
 				// Note that we need to restore the original trailing newlines since
 				// AnalyzeStreamInGroups expects them and they are not present in the GCP
 				// log stream.
-				logLine, _ := logs.ParseLogLineWithPrefix("", in.Content+"\n", nil)
+				logLine, _ := parser.ParseLine(in.Content + "\n")
 				logLine.OccurredAt = in.OccurredAt
 
 				// Ignore loglines which are outside our time window
@@ -200,19 +218,14 @@ func setupLogTransformer(ctx context.Context, wg *sync.WaitGroup, servers []*sta
 					continue
 				}
 
-				for _, server := range servers {
-					if in.GcpProjectID == server.Config.GcpProjectID && in.GcpCloudSQLInstanceID != "" && in.GcpCloudSQLInstanceID == server.Config.GcpCloudSQLInstanceID {
-						out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
-					}
-					if in.GcpProjectID == server.Config.GcpProjectID && in.GcpAlloyDBClusterID != "" && in.GcpAlloyDBClusterID == server.Config.GcpAlloyDBClusterID && in.GcpAlloyDBInstanceID != "" && in.GcpAlloyDBInstanceID == server.Config.GcpAlloyDBInstanceID {
-						// AlloyDB adds a special [filename:lineno] prefix to all log lines (not part of log_line_prefix)
-						parts := regexp.MustCompile(`(?s)^\[[\w.-]+:\d+\]  (.*)`).FindStringSubmatch(string(logLine.Content))
-						if len(parts) == 2 {
-							logLine.Content = parts[1]
-						}
-						out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
+				if isAlloyDBCluster {
+					// AlloyDB adds a special [filename:lineno] prefix to all log lines (not part of log_line_prefix)
+					parts := regexp.MustCompile(`(?s)^\[[\w.-]+:\d+\]  (.*)`).FindStringSubmatch(string(logLine.Content))
+					if len(parts) == 2 {
+						logLine.Content = parts[1]
 					}
 				}
+				out <- state.ParsedLogStreamItem{Identifier: server.Config.Identifier, LogLine: logLine}
 			}
 		}
 	}()

commit 3e611ac62fec3ca69c8cd9705c7299d65240bf0e

Drop special-casing of syslog format
This was a bug introduced in #494. The code here was intended to pass
syslog-format messages to the old parsing code, but the old parsing
code did not actually use this code path for syslog. All the
syslog-specific processing is actually in
input/system/selfhosted/syslog_handler.go, and then the messages are
handed off to the parser. They will be matched against log_line_prefix
like any other log line.

Before #494 this worked fine, because #145 introduced a couple of
log_line_prefix options that could be used with this format.

The RsyslogRegexp was actually introduced in
73712dd996be4485b49ef79df5e5533131020ac9 (before our workflow used
pull requests consistently), and appears to have been intended to tail
log files in an older syslog format (the obsolete RFC3164, versus the
RFC5424 we currently support).

Drop mistaken delegating of syslog message handling to obsolete
parsing mechanism. Also drop this mechanism: it was broken by #494
anyway, and given it's a legacy format (RFC5424 came out in 2009),
there's no reason to believe it's still in use.

input/system/heroku/logs.go
@@ -118,7 +118,7 @@ func processSystemMetrics(ctx context.Context, timestamp time.Time, content []by
 	}
 }
 
-var HerokuLogParser = logs.NewLogParser(logs.LogPrefixHeroku2, nil, false)
+var HerokuLogParser = logs.NewLogParser(logs.LogPrefixHeroku2, nil)
 
 func logStreamItemToLogLine(ctx context.Context, item HttpSyslogMessage, servers []*state.Server, sourceToServer map[string]*state.Server, now time.Time, globalCollectionOpts state.CollectionOpts, logger *util.Logger) (map[string]*state.Server, *state.LogLine, string) {
 	timestamp, err := time.Parse(time.RFC3339, item.HeaderTimestamp)

logs/parse.go
@@ -183,9 +183,8 @@ var EscapeMatchers = map[rune]PrefixEscape{
 }
 
 type LogParser struct {
-	prefix   string
-	tz       *time.Location
-	isSyslog bool
+	prefix string
+	tz     *time.Location
 
 	lineRegexp     *regexp.Regexp
 	prefixElements []PrefixEscape
@@ -193,14 +192,13 @@ type LogParser struct {
 	lineRegexpWithoutLogLevel *regexp.Regexp
 }
 
-func NewLogParser(prefix string, tz *time.Location, isSyslog bool) *LogParser {
+func NewLogParser(prefix string, tz *time.Location) *LogParser {
 	prefixRegexp, prefixElements := parsePrefix(prefix)
 	lineRegexp := regexp.MustCompile("(?ms)^" + prefixRegexp + `(DEBUG|INFO|NOTICE|WARNING|ERROR|LOG|FATAL|PANIC|DETAIL|HINT|CONTEXT|STATEMENT|QUERY):\s+(.*\n?)$`)
 	lineRegexpWithoutLogLevel := regexp.MustCompile("(?ms)^" + prefixRegexp + `(.*\n?)$`)
 	return &LogParser{
-		prefix:   prefix,
-		tz:       tz,
-		isSyslog: isSyslog,
+		prefix: prefix,
+		tz:     tz,
 
 		lineRegexp:     lineRegexp,
 		prefixElements: prefixElements,
@@ -232,8 +230,7 @@ func SyncLogParser(server *state.Server, settings []state.PostgresSetting) {
 	server.LogParseMutex.RLock()
 
 	tz, prefix := getLogConfigFromSettings(settings)
-	isSyslog := server.Config.LogSyslogServer != ""
-	parserInSync := server.LogParser != nil && server.LogParser.Matches(prefix, tz, isSyslog)
+	parserInSync := server.LogParser != nil && server.LogParser.Matches(prefix, tz)
 	server.LogParseMutex.RUnlock()
 
 	if parserInSync {
@@ -243,7 +240,7 @@ func SyncLogParser(server *state.Server, settings []state.PostgresSetting) {
 	server.LogParseMutex.Lock()
 	defer server.LogParseMutex.Unlock()
 
-	server.LogParser = NewLogParser(prefix, tz, isSyslog)
+	server.LogParser = NewLogParser(prefix, tz)
 }
 
 func (lp *LogParser) ValidatePrefix() error {
@@ -266,12 +263,12 @@ func (lp *LogParser) ValidatePrefix() error {
 	}
 }
 
-func (lp *LogParser) Matches(prefix string, tz *time.Location, isSyslog bool) bool {
-	return lp.prefix == prefix && tz.String() == lp.tz.String() && lp.isSyslog == isSyslog
+func (lp *LogParser) Matches(prefix string, tz *time.Location) bool {
+	return lp.prefix == prefix && tz.String() == lp.tz.String()
 }
 
 func (lp *LogParser) GetOccurredAt(timePart string) time.Time {
-	if lp.tz != nil && !lp.isSyslog {
+	if lp.tz != nil {
 		lastSpaceIdx := strings.LastIndex(timePart, " ")
 		if lastSpaceIdx == -1 {
 			return time.Time{}
@@ -287,13 +284,8 @@ func (lp *LogParser) GetOccurredAt(timePart string) time.Time {
 
 	// Assume Postgres time format unless overriden by the prefix (e.g. syslog)
 	var timeFormat, timeFormatAlt string
-	if lp.isSyslog {
-		timeFormat = "2006 Jan  2 15:04:05"
-		timeFormatAlt = ""
-	} else {
-		timeFormat = "2006-01-02 15:04:05 -0700"
-		timeFormatAlt = "2006-01-02 15:04:05 MST"
-	}
+	timeFormat = "2006-01-02 15:04:05 -0700"
+	timeFormatAlt = "2006-01-02 15:04:05 MST"
 
 	ts, err := time.Parse(timeFormat, timePart)
 	if err != nil {
@@ -333,81 +325,7 @@ func (lp *LogParser) GetOccurredAt(timePart string) time.Time {
 	return ts
 }
 
-var UserRegexp = `(\S*)`                              // %u
-var DbRegexp = `(\S*)`                                // %d
-var AppInsideBracketsRegexp = `(\[unknown\]|[^,\]]*)` // %a
-var PidRegexp = `(\d+)`                               // %p
-
-var SyslogSequenceAndSplitRegexp = `(\[[\d-]+\])?`
-var LevelAndContentRegexp = `(\w+):\s+(.*\n?)$`
-var LogPrefixNoTimestampUserDatabaseAppRegexp = regexp.MustCompile(`(?s)^\[user=` + UserRegexp + `,db=` + DbRegexp + `,app=` + AppInsideBracketsRegexp + `\] ` + LevelAndContentRegexp)
-
-var RsyslogLevelAndContentRegexp = `(?:(\w+):\s+)?(.*\n?)$`
-var RsyslogTimeRegexp = `(\w+\s+\d+ \d{2}:\d{2}:\d{2})`
-var RsyslogHostnameRegxp = `(\S+)`
-var RsyslogProcessNameRegexp = `(\w+)`
-var RsyslogRegexp = regexp.MustCompile(`^` + RsyslogTimeRegexp + ` ` + RsyslogHostnameRegxp + ` ` + RsyslogProcessNameRegexp + `\[` + PidRegexp + `\]: ` + SyslogSequenceAndSplitRegexp + ` ` + RsyslogLevelAndContentRegexp)
-
-func (lp *LogParser) parseSyslogLine(line string) (logLine state.LogLine, ok bool) {
-	parts := RsyslogRegexp.FindStringSubmatch(line)
-	if len(parts) == 0 {
-		return
-	}
-
-	timePart := fmt.Sprintf("%d %s", time.Now().Year(), parts[1])
-	// ignore syslog hostname
-	// ignore syslog process name
-	pidPart := parts[4]
-	// ignore syslog postgres sequence and split number
-	levelPart := parts[6]
-	contentPart := strings.Replace(parts[7], "#011", "\t", -1)
-
-	parts = LogPrefixNoTimestampUserDatabaseAppRegexp.FindStringSubmatch(contentPart)
-	if len(parts) == 6 {
-		userPart := parts[1]
-		dbPart := parts[2]
-		appPart := parts[3]
-		levelPart = parts[4]
-		contentPart = parts[5]
-
-		if userPart != "[unknown]" {
-			logLine.Username = userPart
-		}
-		if dbPart != "[unknown]" {
-			logLine.Database = dbPart
-		}
-		if appPart != "[unknown]" {
-			logLine.Application = appPart
-		}
-	}
-
-	occurredAt := lp.GetOccurredAt(timePart)
-	if occurredAt.IsZero() {
-		return
-	}
-
-	logLine.OccurredAt = occurredAt
-
-	backendPid, _ := strconv.ParseInt(pidPart, 10, 32)
-	logLine.BackendPid = int32(backendPid)
-	logLine.Content = contentPart
-
-	// This is actually a continuation of a previous line
-	if levelPart == "" {
-		return
-	}
-
-	logLine.LogLevel = pganalyze_collector.LogLineInformation_LogLevel(pganalyze_collector.LogLineInformation_LogLevel_value[levelPart])
-	ok = true
-
-	return
-}
-
 func (lp *LogParser) ParseLine(line string) (logLine state.LogLine, ok bool) {
-	if lp.isSyslog {
-		return lp.parseSyslogLine(line)
-	}
-
 	if lp.prefix == "" {
 		return logLine, false
 	}

main.go
@@ -214,7 +214,7 @@ func main() {
 			fmt.Println("ERROR: must specify log_line_prefix used to generate logfile with --analyze-logfile-prefix")
 			return
 		}
-		server.LogParser = logs.NewLogParser(analyzeLogfilePrefix, tz, false)
+		server.LogParser = logs.NewLogParser(analyzeLogfilePrefix, tz)
 
 		logLines, samples := logs.ParseAndAnalyzeBuffer(logReader, time.Time{}, server)
 		logs.PrintDebugInfo(logLines, samples)

state/state.go
@@ -343,7 +343,7 @@ const (
 )
 
 type LogParser interface {
-	Matches(prefix string, tz *time.Location, isSyslog bool) bool
+	Matches(prefix string, tz *time.Location) bool
 	GetOccurredAt(timePart string) time.Time
 	ParseLine(line string) (logLine LogLine, ok bool)
 	ValidatePrefix() error

input/system/azure/logs_test.go
@@ -350,7 +350,7 @@ func TestParseRecordToLogLines(t *testing.T) {
 		} else {
 			prefix = logs.LogPrefixAzure
 		}
-		parser := logs.NewLogParser(prefix, nil, false)
+		parser := logs.NewLogParser(prefix, nil)
 
 		var record azure.AzurePostgresLogRecord
 		err := json.Unmarshal([]byte(pair.recordIn), &record)

logs/parse_test.go
@@ -29,45 +29,6 @@ func mustTimeLocation(tzStr string) *time.Location {
 var BSTTimeLocation = mustTimeLocation("Europe/London")
 
 var parseTests = []parseTestpair{
-	// rsyslog format
-	{
-		"",
-		"Feb  1 21:48:31 ip-172-31-14-41 postgres[9076]: [3-1] LOG:  database system is ready to accept connections",
-		nil,
-		state.LogLine{
-			OccurredAt: time.Date(time.Now().Year(), time.February, 1, 21, 48, 31, 0, time.UTC),
-			LogLevel:   pganalyze_collector.LogLineInformation_LOG,
-			BackendPid: 9076,
-			Content:    "database system is ready to accept connections",
-		},
-		true,
-	},
-	{
-		"",
-		"Feb  1 21:48:31 ip-172-31-14-41 postgres[9076]: [3-2] #011 something",
-		nil,
-		state.LogLine{
-			OccurredAt: time.Date(time.Now().Year(), time.February, 1, 21, 48, 31, 0, time.UTC),
-			LogLevel:   pganalyze_collector.LogLineInformation_UNKNOWN,
-			BackendPid: 9076,
-			Content:    "\t something",
-		},
-		false,
-	},
-	{
-		"",
-		"Feb  1 21:48:31 ip-172-31-14-41 postgres[123]: [8-1] [user=postgres,db=postgres,app=[unknown]] LOG: connection received: host=[local]",
-		nil,
-		state.LogLine{
-			OccurredAt: time.Date(time.Now().Year(), time.February, 1, 21, 48, 31, 0, time.UTC),
-			LogLevel:   pganalyze_collector.LogLineInformation_LOG,
-			BackendPid: 123,
-			Username:   "postgres",
-			Database:   "postgres",
-			Content:    "connection received: host=[local]",
-		},
-		true,
-	},
 	// Amazon RDS format
 	{
 		logs.LogPrefixAmazonRds,
@@ -658,11 +619,7 @@ var parseTests = []parseTestpair{
 
 func TestLogParser(t *testing.T) {
 	for _, pair := range parseTests {
-		// Syslog format has a separate, fixed prefix, so the prefix argument is
-		// ignored by the parser in that case. We use an empty string to indicate
-		// that this is a syslog test case.
-		isSyslog := pair.prefixIn == ""
-		parser := logs.NewLogParser(pair.prefixIn, pair.lineInTz, isSyslog)
+		parser := logs.NewLogParser(pair.prefixIn, pair.lineInTz)
 		l, lOk := parser.ParseLine(pair.lineIn)
 
 		cfg := pretty.CompareConfig

logs/replace_test.go
@@ -70,7 +70,7 @@ func TestReplaceSecrets(t *testing.T) {
 	for _, pair := range replaceTests {
 		reader := bufio.NewReader(strings.NewReader(pair.input))
 		server := state.MakeServer(config.ServerConfig{}, false)
-		server.LogParser = logs.NewLogParser(logs.LogPrefixAmazonRds, nil, false)
+		server.LogParser = logs.NewLogParser(logs.LogPrefixAmazonRds, nil)
 		logLines, _ := logs.ParseAndAnalyzeBuffer(reader, time.Time{}, server)
 		logs.ReplaceSecrets(logLines, state.ParseFilterLogSecret(pair.filterLogSecret))
 