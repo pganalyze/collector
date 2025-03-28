0fa50ad067e82827b921287a4c726c8a06ac8dbc
Lukas Fittl <lukas@fittl.com>
2025-03-28 15:04:41 -0700

    Address additional review feedback

--- util/helpers/get_stat_statements.sql
+++ util/helpers/get_stat_statements.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FUNCTION pganalyze.get_stat_statements(showtext boolean = true) RETURNS SETOF pg_stat_statements AS
+DROP FUNCTION IF EXISTS pganalyze.get_stat_statements;
+CREATE FUNCTION pganalyze.get_stat_statements(showtext boolean = true) RETURNS SETOF pg_stat_statements AS
 $$
     /* pganalyze-collector */ SELECT * FROM public.pg_stat_statements(showtext);
 $$ LANGUAGE sql VOLATILE SECURITY DEFINER;
