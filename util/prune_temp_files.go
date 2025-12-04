3b8333b3b0a37a7ae8c2612fa4bff96d61851616
Maciek Sakrejda <m.sakrejda@gmail.com>
2025-12-03 19:02:41 -0800

    Drop ownership check per review

--- util/prune_temp_files.go
+++ util/prune_temp_files.go
@@ -2,28 +2,14 @@ package util
 
 import (
 	"os"
-	"os/user"
 	"path"
-	"strconv"
 	"strings"
-	"syscall"
 )
 
 const TempFilePrefix = "pganalyze_collector_"
 
 // Delete any temp files we may have left behind on an unclean shutdown
 func PruneTempFiles(logger *Logger) {
-	usr, err := user.Current()
-	if err != nil {
-		logger.PrintWarning("Could not check current user to prune temp files: %s\n", err)
-		return
-	}
-	uid, err := strconv.ParseUint(usr.Uid, 10, 32)
-	if err != nil {
-		logger.PrintWarning("Could not parse current user uid to prune temp files: %s\n", err)
-		return
-	}
-	uid32 := uint32(uid)
 	tmpdir := os.TempDir()
 	entries, err := os.ReadDir(tmpdir)
 	if err != nil {
@@ -35,20 +21,11 @@ func PruneTempFiles(logger *Logger) {
 		if !strings.HasPrefix(name, TempFilePrefix) {
 			continue
 		}
-		fi, err := entry.Info()
+		err = os.Remove(path.Join(tmpdir, name))
 		if err != nil {
-			logger.PrintWarning("Could not check info for file %s in temp dir %s: %s\n", name, tmpdir, err)
+			logger.PrintWarning("Could not remove stray temp file %s in temp dir %s: %s\n", name, tmpdir, err)
 			continue
 		}
-		if stat, ok := fi.Sys().(*syscall.Stat_t); ok {
-			if stat.Uid == uid32 {
-				err = os.Remove(path.Join(tmpdir, name))
-				if err != nil {
-					logger.PrintWarning("Could not remove stray temp file %s in temp dir %s: %s\n", name, tmpdir, err)
-					continue
-				}
-				logger.PrintVerbose("Removed stray temp file %s in temp dir %s\n", name, tmpdir)
-			}
-		}
+		logger.PrintVerbose("Removed stray temp file %s in temp dir %s\n", name, tmpdir)
 	}
 }
