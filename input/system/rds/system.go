bf08a8adc2833f945c778cba8d2f621ea0f5c96a
Bison Hubert <bisonhubert@gmail.com>
2026-07-09 15:24:17 -0700

    Combine arn account num with cluster id

--- input/system/rds/system.go
+++ input/system/rds/system.go
@@ -39,7 +39,14 @@ func GetSystemState(server *state.Server, logger *util.Logger) (system state.Sys
 		return
 	}
 
-	system.Info.ClusterID = util.StringPtrToString(instance.DBClusterIdentifier)
+	clusterID := util.StringPtrToString(instance.DBClusterIdentifier)
+	if clusterID != "" {
+		arnParts := strings.Split(util.StringPtrToString(instance.DBInstanceArn), ":")
+		if len(arnParts) >= 5 && arnParts[4] != "" {
+			clusterID = arnParts[4] + "/" + clusterID
+		}
+	}
+	system.Info.ClusterID = clusterID
 
 	isAurora := util.StringPtrToString(instance.Engine) == "aurora-postgresql"
 
